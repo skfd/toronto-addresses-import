2144818eeb3332b50cb857cd5c49fdb71a417498
skfd <kk@comentality.com>
2026-02-23 17:39:52 -0500

    2026-02-23

---
+++ docs/reports/report-2026-02-23-data.js
@@ -0,0 +1,352 @@
+window.REPORT_DATA = {
+  "generated": "Feb 23, 2026 at 05:38 PM",
+  "old_snapshot": {
+    "id": 5,
+    "downloaded": "2026-02-21T00:14:36.723538",
+    "row_count": 525346,
+    "filename": "address-points-2026-02-20.geojson",
+    "remote_last_modified": null,
+    "remote_content_length": null,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 6,
+    "downloaded": "2026-02-23T17:38:17.717135",
+    "row_count": 525347,
+    "filename": "address-points-2026-02-23.geojson",
+    "remote_last_modified": "Mon, 23 Feb 2026 11:39:42 GMT",
+    "remote_content_length": 589638796,
+    "skipped": 0
+  },
+  "old_date_friendly": "Saturday, Feb 21, 2026",
+  "new_date_friendly": "Monday, Feb 23, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 6,
+      "max_snapshot_id": 6,
+      "address_point_id": 60055877,
+      "address_full": "619B Mount Pleasant Rd",
+      "address_number": "619B",
+      "lo_num": 619,
+      "lo_num_suf": "B",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Mount Pleasant Rd",
+      "linear_name": "Mount Pleasant",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Don Valley West",
+      "longitude": -79.38829,
+      "latitude": 43.7042,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007297, \"ADDRESS_ID_LINK\": 110707, \"ADDRESS_POINT_ID_LINK\": 844933, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006214, \"CENTRELINE_ID\": 1138526, \"CENTRELINE_MEASURE\": 99.35, \"CENTRELINE_OFFSET\": 17.15, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4022.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5658273\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"15\"}"
+    },
+    {
+      "min_snapshot_id": 6,
+      "max_snapshot_id": 6,
+      "address_point_id": 60055878,
+      "address_full": "619R Mount Pleasant Rd",
+      "address_number": "619R",
+      "lo_num": 619,
+      "lo_num_suf": "R",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Mount Pleasant Rd",
+      "linear_name": "Mount Pleasant",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Don Valley West",
+      "longitude": -79.38805,
+      "latitude": 43.70423,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007299, \"ADDRESS_ID_LINK\": 110707, \"ADDRESS_POINT_ID_LINK\": 844933, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006215, \"CENTRELINE_ID\": 1138526, \"CENTRELINE_MEASURE\": 98.19, \"CENTRELINE_OFFSET\": 37.2, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4022.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5658274\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"15\"}"
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 3,
+      "max_snapshot_id": 5,
+      "address_point_id": 1372840,
+      "address_full": "4 Vansco Rd",
+      "address_number": "4",
+      "lo_num": 4,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Vansco Rd",
+      "linear_name": "Vansco",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.53433,
+      "latitude": 43.61963,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 9778, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 630008, \"CENTRELINE_ID\": 913562, \"CENTRELINE_MEASURE\": -2.81, \"CENTRELINE_OFFSET\": 18.46, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2709.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"5580884\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    }
+  ],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 9779422,
+      "address_full": "1 Riverdale Dr",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.72934,
+      "longitude": -79.55861,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72933, -79.55864",
+          "new": "43.72934, -79.55861 \u2197 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 1009563,
+      "address_full": "19 Jason Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.72885,
+      "longitude": -79.55771,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72908, -79.55764",
+          "new": "43.72885, -79.55771 \u2193 26.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 1009566,
+      "address_full": "23 Jason Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.72891,
+      "longitude": -79.55786,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72904, -79.55787",
+          "new": "43.72891, -79.55786 \u2193 14.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 1009571,
+      "address_full": "34 Jason Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.72918,
+      "longitude": -79.55864,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72916, -79.55873",
+          "new": "43.72918, -79.55864 \u2192 7.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9847272,
+      "address_full": "3 Gibson Ave",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.73075,
+      "longitude": -79.55585,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73074, -79.55593",
+          "new": "43.73075, -79.55585 \u2192 6.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9779429,
+      "address_full": "12 Riverdale Dr",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.7296,
+      "longitude": -79.55969,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72962, -79.55978",
+          "new": "43.72960, -79.55969 \u2192 7.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14194646,
+      "address_full": "46 Acara Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.78342,
+      "longitude": -79.31508,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.78345, -79.31495",
+          "new": "43.78342, -79.31508 \u2190 11.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 300459,
+      "address_full": "16 Agincourt Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.78552,
+      "longitude": -79.28324,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.78547, -79.28344",
+          "new": "43.78552, -79.28324 \u2192 17.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7009484,
+      "address_full": "5 Ross Ave",
+      "municipality_name": "Scarborough",
+      "latitude": 43.78611,
+      "longitude": -79.2822,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.78601, -79.28220",
+          "new": "43.78611, -79.28220 \u2191 11.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 557884,
+      "address_full": "18 Roycrest Ave",
+      "municipality_name": "North York",
+      "latitude": 43.75857,
+      "longitude": -79.42791,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75860, -79.42795",
+          "new": "43.75857, -79.42791 \u2198 4.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 844933,
+      "address_full": "619 Mount Pleasant Rd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.70418,
+      "longitude": -79.38828,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70422, -79.38823",
+          "new": "43.70418, -79.38828 \u2199 6.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 844935,
+      "address_full": "621 Mount Pleasant Rd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.70424,
+      "longitude": -79.38832,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70427, -79.38819",
+          "new": "43.70424, -79.38832 \u2190 11.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14208142,
+      "address_full": "621A Mount Pleasant Rd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.70428,
+      "longitude": -79.38814,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70426, -79.38813",
+          "new": "43.70428, -79.38814 \u2191 2.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 508083,
+      "address_full": "33 Don River Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75826,
+      "longitude": -79.43201,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75825, -79.43215",
+          "new": "43.75826, -79.43201 \u2192 11.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 996560,
+      "address_full": "16 Dixington Cres",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.69695,
+      "longitude": -79.54339,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.69697, -79.54333",
+          "new": "43.69695, -79.54339 \u2199 5.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 996570,
+      "address_full": "29 Dixington Cres",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.69701,
+      "longitude": -79.54184,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.69699, -79.54188",
+          "new": "43.69701, -79.54184 \u2197 3.9m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 2
+    },
+    "muni_removed": {
+      "Etobicoke": 1
+    },
+    "ward_added": {
+      "Don Valley West": 2
+    },
+    "ward_removed": {
+      "Etobicoke-Lakeshore": 1
+    },
+    "field_changes": {
+      "Location (latitude)": 16,
+      "Location (longitude)": 15
+    }
+  },
+  "added_count": 2,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 16
+}
