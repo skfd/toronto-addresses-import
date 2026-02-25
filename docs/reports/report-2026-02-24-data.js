cc862b8b9d304f8a712f61992085f75d258de405
skfd <kk@comentality.com>
2026-02-25 01:29:46 -0500

    data up

---
+++ docs/reports/report-2026-02-24-data.js
@@ -0,0 +1,138 @@
+window.REPORT_DATA = {
+  "generated": "Feb 24, 2026 at 02:46 PM",
+  "old_snapshot": {
+    "id": 6,
+    "downloaded": "2026-02-23T17:38:17.717135",
+    "row_count": 525347,
+    "filename": "address-points-2026-02-23.geojson",
+    "remote_last_modified": "Mon, 23 Feb 2026 11:39:42 GMT",
+    "remote_content_length": 589638796,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 7,
+    "downloaded": "2026-02-24T14:46:14.805556",
+    "row_count": 525347,
+    "filename": "address-points-2026-02-24.geojson",
+    "remote_last_modified": "Tue, 24 Feb 2026 11:37:30 GMT",
+    "remote_content_length": 589639078,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, Feb 23, 2026",
+  "new_date_friendly": "Tuesday, Feb 24, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 761360,
+      "address_full": "102 Albany Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66863,
+      "longitude": -79.41152,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66862, -79.41149",
+          "new": "43.66863, -79.41152",
+          "arrow": "\u2196 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30033179,
+      "address_full": "19 Alvin Curling Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.82062,
+      "longitude": -79.27775,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.82055, -79.27769",
+          "new": "43.82062, -79.27775",
+          "arrow": "\u2196 9.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30033141,
+      "address_full": "9 Serenity Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.81952,
+      "longitude": -79.27774,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.81956, -79.27772",
+          "new": "43.81952, -79.27774",
+          "arrow": "\u2193 4.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30033140,
+      "address_full": "11 Serenity Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.81958,
+      "longitude": -79.27758,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.81961, -79.27759",
+          "new": "43.81958, -79.27758",
+          "arrow": "\u2193 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30032324,
+      "address_full": "8 Serenity Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.8199,
+      "longitude": -79.27806,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.81986, -79.27803",
+          "new": "43.81990, -79.27806",
+          "arrow": "\u2196 5.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30032322,
+      "address_full": "7 Serenity Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.81949,
+      "longitude": -79.27787,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.81952, -79.27788",
+          "new": "43.81949, -79.27787",
+          "arrow": "\u2193 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 6,
+      "Location (latitude)": 6
+    }
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 6
+}
---
+++ docs/reports/report-2026-02-25-skipped-data.js
@@ -0,0 +1,25 @@
+window.REPORT_DATA = {
+  "generated": "Feb 25, 2026 at 01:28 AM",
+  "old_snapshot": {
+    "downloaded": "2026-02-25T01:28:25.417305",
+    "filename": "No New Data",
+    "id": "-",
+    "row_count": 525347
+  },
+  "new_snapshot": {
+    "downloaded": "2026-02-25T01:28:25.417305",
+    "filename": "No New Data",
+    "id": "-",
+    "row_count": 525347
+  },
+  "old_date_friendly": "Wednesday, Feb 25, 2026",
+  "new_date_friendly": "Wednesday, Feb 25, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "stats": {},
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "is_skipped": true
+}
