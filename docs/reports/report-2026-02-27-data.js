363694e4689b408a4aba0e44f44c9baf68adf134
skfd <kk@comentality.com>
2026-02-28 03:00:30 -0500

    data update

---
+++ docs/reports/report-2026-02-27-data.js
@@ -0,0 +1,438 @@
+window.REPORT_DATA = {
+  "generated": "Feb 28, 2026 at 02:59 AM",
+  "old_snapshot": {
+    "id": 7,
+    "downloaded": "2026-02-24T14:46:14.805556",
+    "row_count": 525347,
+    "filename": "address-points-2026-02-24.geojson",
+    "remote_last_modified": "Tue, 24 Feb 2026 11:37:30 GMT",
+    "remote_content_length": 589639078,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 9,
+    "downloaded": "2026-02-28T02:58:55.680798",
+    "row_count": 525359,
+    "filename": "address-points-2026-02-27.geojson",
+    "remote_last_modified": "Fri, 27 Feb 2026 11:38:30 GMT",
+    "remote_content_length": 589652488,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Feb 24, 2026",
+  "new_date_friendly": "Saturday, Feb 28, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056291,
+      "address_full": "34 Fairfield Ave",
+      "address_number": "34",
+      "lo_num": 34,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Fairfield Ave",
+      "linear_name": "Fairfield",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.5257,
+      "latitude": 43.59739,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007302, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 4257, \"CENTRELINE_ID\": 914631, \"CENTRELINE_MEASURE\": 72.17, \"CENTRELINE_OFFSET\": 23.21, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1582.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"5661384\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056292,
+      "address_full": "36 Fairfield Ave",
+      "address_number": "36",
+      "lo_num": 36,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Fairfield Ave",
+      "linear_name": "Fairfield",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.52579,
+      "latitude": 43.59738,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007303, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006216, \"CENTRELINE_ID\": 914631, \"CENTRELINE_MEASURE\": 78.84, \"CENTRELINE_OFFSET\": 24.62, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1582.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"5661385\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056323,
+      "address_full": "400R Unwin Ave",
+      "address_number": "400R",
+      "lo_num": 400,
+      "lo_num_suf": "R",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Unwin Ave",
+      "linear_name": "Unwin",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-Danforth",
+      "longitude": -79.33787,
+      "latitude": 43.64586,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007309, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006222, \"CENTRELINE_ID\": 30075996, \"CENTRELINE_MEASURE\": 371.76, \"CENTRELINE_OFFSET\": 162.25, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4587.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661542\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"14\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056324,
+      "address_full": "23 Pura Vida Lane",
+      "address_number": "23",
+      "lo_num": 23,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Pura Vida Lane",
+      "linear_name": "Pura Vida",
+      "linear_name_type": "Lane",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Davenport",
+      "longitude": -79.42981,
+      "latitude": 43.66711,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007315, \"ADDRESS_ID_LINK\": 136290, \"ADDRESS_POINT_ID_LINK\": 10872853, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006217, \"CENTRELINE_ID\": 14047457, \"CENTRELINE_MEASURE\": 6.85, \"CENTRELINE_OFFSET\": 4.37, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 30204.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661558\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"09\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056325,
+      "address_full": "2271 Bloor St W",
+      "address_number": "2271",
+      "lo_num": 2271,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bloor St W",
+      "linear_name": "Bloor",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "former Toronto",
+      "ward_name": "Parkdale-High Park",
+      "longitude": -79.47835,
+      "latitude": 43.65045,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007319, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 183985, \"CENTRELINE_ID\": 9909993, \"CENTRELINE_MEASURE\": 26.9, \"CENTRELINE_OFFSET\": 24.89, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 458.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661568\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"04\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056326,
+      "address_full": "2273 Bloor St W",
+      "address_number": "2273",
+      "lo_num": 2273,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bloor St W",
+      "linear_name": "Bloor",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "former Toronto",
+      "ward_name": "Parkdale-High Park",
+      "longitude": -79.47845,
+      "latitude": 43.65047,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007320, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 183988, \"CENTRELINE_ID\": 9909993, \"CENTRELINE_MEASURE\": 33.61, \"CENTRELINE_OFFSET\": 20.88, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 458.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661569\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"04\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056333,
+      "address_full": "27 Anson Ave",
+      "address_number": "27",
+      "lo_num": 27,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Anson Ave",
+      "linear_name": "Anson",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.23922,
+      "latitude": 43.72488,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007321, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006221, \"CENTRELINE_ID\": 111622, \"CENTRELINE_MEASURE\": 94.54, \"CENTRELINE_OFFSET\": 37.3, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7214.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"5661574\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056334,
+      "address_full": "29 Anson Ave",
+      "address_number": "29",
+      "lo_num": 29,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Anson Ave",
+      "linear_name": "Anson",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.23933,
+      "latitude": 43.72486,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007324, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 334970, \"CENTRELINE_ID\": 111622, \"CENTRELINE_MEASURE\": 85.76, \"CENTRELINE_OFFSET\": 36.54, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7214.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"5661576\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056336,
+      "address_full": "35 Brownlow Ave",
+      "address_number": "35",
+      "lo_num": 35,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Brownlow Ave",
+      "linear_name": "Brownlow",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.39065,
+      "latitude": 43.70679,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007325, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006219, \"CENTRELINE_ID\": 1138355, \"CENTRELINE_MEASURE\": 105.31, \"CENTRELINE_OFFSET\": 23.43, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3074.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661577\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056337,
+      "address_full": "75 Brownlow Ave",
+      "address_number": "75",
+      "lo_num": 75,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Brownlow Ave",
+      "linear_name": "Brownlow",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.39075,
+      "latitude": 43.70712,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007328, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 116619, \"CENTRELINE_ID\": 1138355, \"CENTRELINE_MEASURE\": 142.98, \"CENTRELINE_OFFSET\": 26.27, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3074.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661580\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056338,
+      "address_full": "81 Brownlow Ave",
+      "address_number": "81",
+      "lo_num": 81,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Brownlow Ave",
+      "linear_name": "Brownlow",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.39081,
+      "latitude": 43.7074,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007329, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006220, \"CENTRELINE_ID\": 1138355, \"CENTRELINE_MEASURE\": 174.4, \"CENTRELINE_OFFSET\": 29.84, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3074.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661582\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    },
+    {
+      "min_snapshot_id": 9,
+      "max_snapshot_id": 9,
+      "address_point_id": 60056340,
+      "address_full": "25 Brownlow Ave",
+      "address_number": "25",
+      "lo_num": 25,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Brownlow Ave",
+      "linear_name": "Brownlow",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.39056,
+      "latitude": 43.70651,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007331, \"ADDRESS_ID_LINK\": 60007325, \"ADDRESS_POINT_ID_LINK\": 60056336, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006218, \"CENTRELINE_ID\": 1138355, \"CENTRELINE_MEASURE\": 73.91, \"CENTRELINE_OFFSET\": 23.0, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3074.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"5661583\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 12313164,
+      "address_full": "842 Eglinton Ave W",
+      "municipality_name": "former Toronto",
+      "latitude": 43.7013,
+      "longitude": -79.42532,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70140, -79.42534",
+          "new": "43.70130, -79.42532",
+          "arrow": "\u2193 11.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14068986,
+      "address_full": "300 Unwin Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64294,
+      "longitude": -79.33951,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64493, -79.33606",
+          "new": "43.64294, -79.33951",
+          "arrow": "\u2199 355.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10495095,
+      "address_full": "348 Unwin Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6446,
+      "longitude": -79.3382,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64422, -79.33795",
+          "new": "43.64460, -79.33820",
+          "arrow": "\u2196 46.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10495101,
+      "address_full": "294 Unwin Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64296,
+      "longitude": -79.34041,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64298, -79.34015",
+          "new": "43.64296, -79.34041",
+          "arrow": "\u2190 21.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30041805,
+      "address_full": "779 Crawford St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66464,
+      "longitude": -79.42191,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66491, -79.42273",
+          "new": "43.66464, -79.42191",
+          "arrow": "\u2198 72.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30140814,
+      "address_full": "809 Eglinton Ave E",
+      "municipality_name": "East York",
+      "latitude": 43.71324,
+      "longitude": -79.36515,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71327, -79.36516",
+          "new": "43.71324, -79.36515",
+          "arrow": "\u2193 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9910012,
+      "address_full": "2271A Bloor St W",
+      "municipality_name": "former Toronto",
+      "latitude": 43.65048,
+      "longitude": -79.47836,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.65044, -79.47838",
+          "new": "43.65048, -79.47836",
+          "arrow": "\u2191 4.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10872853,
+      "address_full": "395 Delaware Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66704,
+      "longitude": -79.43014,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66705, -79.43014",
+          "new": "43.66704, -79.43014",
+          "arrow": "\u2193 1.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 800621,
+      "address_full": "223 Eglinton Ave E",
+      "municipality_name": "former Toronto",
+      "latitude": 43.70801,
+      "longitude": -79.39077,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70790, -79.39085",
+          "new": "43.70801, -79.39077",
+          "arrow": "\u2197 13.8m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 8,
+      "Etobicoke": 2,
+      "Scarborough": 2
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Toronto-St. Paul's": 4,
+      "Etobicoke-Lakeshore": 2,
+      "Parkdale-High Park": 2,
+      "Scarborough Southwest": 2,
+      "Toronto-Danforth": 1,
+      "Davenport": 1
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Location (latitude)": 9,
+      "Location (longitude)": 8
+    }
+  },
+  "added_count": 12,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 9
+}
---
+++ docs/reports/report-2026-02-28-skipped-data.js
@@ -0,0 +1,25 @@
+window.REPORT_DATA = {
+  "generated": "Feb 28, 2026 at 03:00 AM",
+  "old_snapshot": {
+    "downloaded": "2026-02-28T03:00:11.289677",
+    "filename": "No New Data",
+    "id": "-",
+    "row_count": 525359
+  },
+  "new_snapshot": {
+    "downloaded": "2026-02-28T03:00:11.289677",
+    "filename": "No New Data",
+    "id": "-",
+    "row_count": 525359
+  },
+  "old_date_friendly": "Saturday, Feb 28, 2026",
+  "new_date_friendly": "Saturday, Feb 28, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "stats": {},
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "is_skipped": true
+}
