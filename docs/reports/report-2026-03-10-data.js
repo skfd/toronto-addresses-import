55738a18bdc8a98598fdfb9c244bce6759e6b7c9
skfd <kk@comentality.com>
2026-03-12 20:57:47 -0400

    data update

---
+++ docs/reports/report-2026-03-10-data.js
@@ -0,0 +1,291 @@
+window.REPORT_DATA = {
+  "generated": "Mar 10, 2026 at 01:30 PM",
+  "old_snapshot": {
+    "id": 13,
+    "downloaded": "2026-03-07T12:00:26.330969",
+    "row_count": 525367,
+    "filename": "address-points-2026-03-07.geojson",
+    "remote_last_modified": "Sat, 07 Mar 2026 11:35:58 GMT",
+    "remote_content_length": 589661748,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 14,
+    "downloaded": "2026-03-10T13:29:14.706454",
+    "row_count": 525366,
+    "filename": "address-points-2026-03-10.geojson",
+    "remote_last_modified": "Tue, 10 Mar 2026 11:38:55 GMT",
+    "remote_content_length": 589660759,
+    "skipped": 0
+  },
+  "old_date_friendly": "Saturday, Mar 07, 2026",
+  "new_date_friendly": "Tuesday, Mar 10, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 2,
+      "max_snapshot_id": 13,
+      "address_point_id": 20040882,
+      "address_full": "194 Hanna Rd",
+      "address_number": "194",
+      "lo_num": 194,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Hanna Rd",
+      "linear_name": "Hanna",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "East York",
+      "ward_name": "Don Valley West",
+      "longitude": -79.37111,
+      "latitude": 43.71088,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1483960, \"ADDRESS_ID_LINK\": 561550, \"ADDRESS_POINT_ID_LINK\": 11915539, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 381829, \"CENTRELINE_ID\": 3244890, \"CENTRELINE_MEASURE\": 177.77, \"CENTRELINE_OFFSET\": 36.35, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 187.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"EY\", \"OBJECTID\": \"1613219\", \"PLACE_NAME\": \"Howard Talbot Park\", \"PLACE_NAME_ALL\": \"Howard Talbot Park\", \"WARD\": \"15\"}"
+    }
+  ],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 20014831,
+      "address_full": "175 Fisherville Rd",
+      "municipality_name": "North York",
+      "latitude": 43.7876,
+      "longitude": -79.45836,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.78637, -79.45786",
+          "new": "43.78760, -79.45836",
+          "arrow": "\u2191 142.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 18931,
+      "address_full": "84 Cameron Cres",
+      "municipality_name": "East York",
+      "latitude": 43.70984,
+      "longitude": -79.37317,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70984, -79.37311",
+          "new": "43.70984, -79.37317",
+          "arrow": "\u2190 4.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14646106,
+      "address_full": "11 London Green Crt",
+      "municipality_name": "North York",
+      "latitude": 43.75384,
+      "longitude": -79.51081,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75437, -79.50908",
+          "new": "43.75384, -79.51081",
+          "arrow": "\u2199 151.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7304898,
+      "address_full": "126 Lake Promenade",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.59071,
+      "longitude": -79.52564,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.59087, -79.52572",
+          "new": "43.59071, -79.52564",
+          "arrow": "\u2193 18.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14077880,
+      "address_full": "299 Pharmacy Ave",
+      "municipality_name": "Scarborough",
+      "latitude": 43.70092,
+      "longitude": -79.28409,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70129, -79.28496",
+          "new": "43.70092, -79.28409",
+          "arrow": "\u2198 81.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30023729,
+      "address_full": "190 Hanna Rd",
+      "municipality_name": "East York",
+      "latitude": 43.71049,
+      "longitude": -79.37113,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71078, -79.37084",
+          "new": "43.71049, -79.37113",
+          "arrow": "\u2199 39.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7323287,
+      "address_full": "75 Arcadian Crcl",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.59194,
+      "longitude": -79.52734,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.59178, -79.52783",
+          "new": "43.59194, -79.52734",
+          "arrow": "\u2197 43.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30035267,
+      "address_full": "110 Eastdale Ave",
+      "municipality_name": "East York",
+      "latitude": 43.69537,
+      "longitude": -79.30145,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.69536, -79.30159",
+          "new": "43.69537, -79.30145",
+          "arrow": "\u2192 11.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7304924,
+      "address_full": "184 Lake Promenade",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.59043,
+      "longitude": -79.52842,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.59047, -79.52849",
+          "new": "43.59043, -79.52842",
+          "arrow": "\u2198 7.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8237178,
+      "address_full": "146 Orchard View Blvd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.70731,
+      "longitude": -79.40361,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70733, -79.40362",
+          "new": "43.70731, -79.40361",
+          "arrow": "\u2193 2.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30069312,
+      "address_full": "262B Bessborough Dr",
+      "municipality_name": "East York",
+      "latitude": 43.70993,
+      "longitude": -79.3717,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70988, -79.37187",
+          "new": "43.70993, -79.37170",
+          "arrow": "\u2192 14.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7553684,
+      "address_full": "651 Fleet St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63589,
+      "longitude": -79.40492,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63589, -79.40478",
+          "new": "43.63589, -79.40492",
+          "arrow": "\u2190 11.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8237358,
+      "address_full": "56 Edith Dr",
+      "municipality_name": "former Toronto",
+      "latitude": 43.70734,
+      "longitude": -79.40357,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70728, -79.40363",
+          "new": "43.70734, -79.40357",
+          "arrow": "\u2197 8.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 13323642,
+      "address_full": "214 Wellington St W",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64567,
+      "longitude": -79.38905,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64552, -79.38910",
+          "new": "43.64567, -79.38905",
+          "arrow": "\u2191 17.2m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "East York": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "Don Valley West": 1
+    },
+    "field_changes": {
+      "Location (longitude)": 14,
+      "Location (latitude)": 12
+    }
+  },
+  "added_count": 0,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 14
+}
---
+++ docs/reports/report-2026-03-12-data.js
@@ -0,0 +1,509 @@
+window.REPORT_DATA = {
+  "generated": "Mar 12, 2026 at 08:57 PM",
+  "old_snapshot": {
+    "id": 14,
+    "downloaded": "2026-03-10T13:29:14.706454",
+    "row_count": 525366,
+    "filename": "address-points-2026-03-10.geojson",
+    "remote_last_modified": "Tue, 10 Mar 2026 11:38:55 GMT",
+    "remote_content_length": 589660759,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 15,
+    "downloaded": "2026-03-12T20:56:53.836630",
+    "row_count": 525370,
+    "filename": "address-points-2026-03-12.geojson",
+    "remote_last_modified": "Thu, 12 Mar 2026 11:39:09 GMT",
+    "remote_content_length": 589665158,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Mar 10, 2026",
+  "new_date_friendly": "Thursday, Mar 12, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 15,
+      "max_snapshot_id": 15,
+      "address_point_id": 60062448,
+      "address_full": "16 Southwell Dr",
+      "address_number": "16",
+      "lo_num": 16,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Southwell Dr",
+      "linear_name": "Southwell",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Don Valley East",
+      "longitude": -79.35708,
+      "latitude": 43.74655,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007450, \"ADDRESS_ID_LINK\": 274534, \"ADDRESS_POINT_ID_LINK\": 563856, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006241, \"CENTRELINE_ID\": 441580, \"CENTRELINE_MEASURE\": 128.13, \"CENTRELINE_OFFSET\": 26.08, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 6753.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"5678828\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"16\"}"
+    },
+    {
+      "min_snapshot_id": 15,
+      "max_snapshot_id": 15,
+      "address_point_id": 60062449,
+      "address_full": "18 Southwell Dr",
+      "address_number": "18",
+      "lo_num": 18,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Southwell Dr",
+      "linear_name": "Southwell",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Don Valley East",
+      "longitude": -79.35704,
+      "latitude": 43.74655,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007452, \"ADDRESS_ID_LINK\": 274534, \"ADDRESS_POINT_ID_LINK\": 563856, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006242, \"CENTRELINE_ID\": 441580, \"CENTRELINE_MEASURE\": 131.05, \"CENTRELINE_OFFSET\": 26.08, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 6753.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"5678830\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"16\"}"
+    },
+    {
+      "min_snapshot_id": 15,
+      "max_snapshot_id": 15,
+      "address_point_id": 60062450,
+      "address_full": "20 Southwell Dr",
+      "address_number": "20",
+      "lo_num": 20,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Southwell Dr",
+      "linear_name": "Southwell",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Don Valley East",
+      "longitude": -79.357,
+      "latitude": 43.74656,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007454, \"ADDRESS_ID_LINK\": 274534, \"ADDRESS_POINT_ID_LINK\": 563856, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006243, \"CENTRELINE_ID\": 441580, \"CENTRELINE_MEASURE\": 134.17, \"CENTRELINE_OFFSET\": 25.89, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 6753.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"5678831\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"16\"}"
+    },
+    {
+      "min_snapshot_id": 15,
+      "max_snapshot_id": 15,
+      "address_point_id": 60062452,
+      "address_full": "140 Bogert Ave",
+      "address_number": "140",
+      "lo_num": 140,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bogert Ave",
+      "linear_name": "Bogert",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Willowdale",
+      "longitude": -79.41755,
+      "latitude": 43.75944,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007456, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 432924, \"CENTRELINE_ID\": 11529919, \"CENTRELINE_MEASURE\": 121.29, \"CENTRELINE_OFFSET\": 23.65, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4992.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"5679027\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"18\"}"
+    },
+    {
+      "min_snapshot_id": 15,
+      "max_snapshot_id": 15,
+      "address_point_id": 60062453,
+      "address_full": "142 Bogert Ave",
+      "address_number": "142",
+      "lo_num": 142,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bogert Ave",
+      "linear_name": "Bogert",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Willowdale",
+      "longitude": -79.41762,
+      "latitude": 43.75943,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007457, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006244, \"CENTRELINE_ID\": 11529919, \"CENTRELINE_MEASURE\": 126.91, \"CENTRELINE_OFFSET\": 23.98, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4992.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"5679029\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"18\"}"
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 2,
+      "max_snapshot_id": 14,
+      "address_point_id": 9135123,
+      "address_full": "298 Manse Rd",
+      "address_number": "298",
+      "lo_num": 298,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Manse Rd",
+      "linear_name": "Manse",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough-Rouge Park",
+      "longitude": -79.17623,
+      "latitude": 43.76919,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1298455, \"ADDRESS_ID_LINK\": 812249, \"ADDRESS_POINT_ID_LINK\": 9135077, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 345922, \"CENTRELINE_ID\": 9135125, \"CENTRELINE_MEASURE\": 95.17, \"CENTRELINE_OFFSET\": 38.03, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 8411.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"3040637\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"25\"}"
+    }
+  ],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 398444,
+      "address_full": "30 Willowmount Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.7169,
+      "longitude": -79.27569,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71694, -79.27571",
+          "new": "43.71690, -79.27569",
+          "arrow": "\u2193 4.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 398452,
+      "address_full": "38 Willowmount Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71716,
+      "longitude": -79.27536,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71718, -79.27534",
+          "new": "43.71716, -79.27536",
+          "arrow": "\u2199 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 398437,
+      "address_full": "23 Willowmount Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71668,
+      "longitude": -79.27519,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71673, -79.27522",
+          "new": "43.71668, -79.27519",
+          "arrow": "\u2198 6.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 319450,
+      "address_full": "74 Chestnut Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71595,
+      "longitude": -79.27707,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71597, -79.27706",
+          "new": "43.71595, -79.27707",
+          "arrow": "\u2193 2.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 319435,
+      "address_full": "49 Chestnut Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71576,
+      "longitude": -79.27616,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71577, -79.27610",
+          "new": "43.71576, -79.27616",
+          "arrow": "\u2190 5.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 319455,
+      "address_full": "79 Chestnut Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71604,
+      "longitude": -79.2765,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71608, -79.27647",
+          "new": "43.71604, -79.27650",
+          "arrow": "\u2199 5.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 319445,
+      "address_full": "66 Chestnut Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71561,
+      "longitude": -79.27696,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71559, -79.27696",
+          "new": "43.71561, -79.27696",
+          "arrow": "\u2191 2.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 319447,
+      "address_full": "70 Chestnut Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71578,
+      "longitude": -79.27703,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71575, -79.27708",
+          "new": "43.71578, -79.27703",
+          "arrow": "\u2197 5.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 319448,
+      "address_full": "72 Chestnut Cres",
+      "municipality_name": "Scarborough",
+      "latitude": 43.71589,
+      "longitude": -79.27708,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71591, -79.27710",
+          "new": "43.71589, -79.27708",
+          "arrow": "\u2198 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 545041,
+      "address_full": "53 Neptune Dr",
+      "municipality_name": "North York",
+      "latitude": 43.73168,
+      "longitude": -79.43562,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73164, -79.43578",
+          "new": "43.73168, -79.43562",
+          "arrow": "\u2192 13.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122788,
+      "address_full": "1 Liam Foudy Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75538,
+      "longitude": -79.26529,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75540, -79.26526",
+          "new": "43.75538, -79.26529",
+          "arrow": "\u2199 3.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122787,
+      "address_full": "165 Brockley Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.7554,
+      "longitude": -79.26512,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75539, -79.26508",
+          "new": "43.75540, -79.26512",
+          "arrow": "\u2190 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122786,
+      "address_full": "163 Brockley Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.7554,
+      "longitude": -79.26505,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75540, -79.26501",
+          "new": "43.75540, -79.26505",
+          "arrow": "\u2190 3.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122785,
+      "address_full": "159 Brockley Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75542,
+      "longitude": -79.26491,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75543, -79.26488",
+          "new": "43.75542, -79.26491",
+          "arrow": "\u2199 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122783,
+      "address_full": "161 Brockley Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75542,
+      "longitude": -79.26498,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75542, -79.26494",
+          "new": "43.75542, -79.26498",
+          "arrow": "\u2190 3.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122782,
+      "address_full": "167 Brockley Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75539,
+      "longitude": -79.2652,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75537, -79.26515",
+          "new": "43.75539, -79.26520",
+          "arrow": "\u2196 4.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122773,
+      "address_full": "6 Liam Foudy Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75541,
+      "longitude": -79.26567,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75543, -79.26567",
+          "new": "43.75541, -79.26567",
+          "arrow": "\u2193 2.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 563856,
+      "address_full": "12 Southwell Dr",
+      "municipality_name": "North York",
+      "latitude": 43.74654,
+      "longitude": -79.35711,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.74659, -79.35698",
+          "new": "43.74654, -79.35711",
+          "arrow": "\u2199 11.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10280124,
+      "address_full": "4777 Dufferin St",
+      "municipality_name": "North York",
+      "latitude": 43.77714,
+      "longitude": -79.46182,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.77754, -79.46856",
+          "new": "43.77714, -79.46182",
+          "arrow": "\u2192 543.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8120455,
+      "address_full": "42 Mercury Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.72125,
+      "longitude": -79.59136,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72102, -79.59141",
+          "new": "43.72125, -79.59136",
+          "arrow": "\u2191 25.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30122772,
+      "address_full": "8 Liam Foudy Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75546,
+      "longitude": -79.26569,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75549, -79.26570",
+          "new": "43.75546, -79.26569",
+          "arrow": "\u2193 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "North York": 5
+    },
+    "muni_removed": {
+      "Scarborough": 1
+    },
+    "ward_added": {
+      "Don Valley East": 3,
+      "Willowdale": 2
+    },
+    "ward_removed": {
+      "Scarborough-Rouge Park": 1
+    },
+    "field_changes": {
+      "Location (longitude)": 19,
+      "Location (latitude)": 19
+    }
+  },
+  "added_count": 5,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 21
+}
