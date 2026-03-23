5c934866e934a3517d3c28d3b5165c236dd632b3
skfd <kk@comentality.com>
2026-03-23 13:00:56 -0400

    data 2026-03-23

---
+++ docs/reports/report-2026-03-23-data.js
@@ -0,0 +1,154 @@
+window.REPORT_DATA = {
+  "generated": "Mar 23, 2026 at 01:00 PM",
+  "old_snapshot": {
+    "id": 17,
+    "downloaded": "2026-03-21T16:22:53.531235",
+    "row_count": 525379,
+    "filename": "address-points-2026-03-20.geojson",
+    "remote_last_modified": "Fri, 20 Mar 2026 11:38:56 GMT",
+    "remote_content_length": 589181603,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 18,
+    "downloaded": "2026-03-23T13:00:10.252581",
+    "row_count": 525379,
+    "filename": "address-points-2026-03-23.geojson",
+    "remote_last_modified": "Mon, 23 Mar 2026 11:40:28 GMT",
+    "remote_content_length": 589181602,
+    "skipped": 0
+  },
+  "old_date_friendly": "Saturday, Mar 21, 2026",
+  "new_date_friendly": "Monday, Mar 23, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 6807205,
+      "address_full": "37 Marquis Ave",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.65835,
+      "longitude": -79.50788,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.65792, -79.50859",
+          "new": "43.65835, -79.50788",
+          "arrow": "\u2197 74.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 982436,
+      "address_full": "1525 Albion Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.7402,
+      "longitude": -79.58552,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.74027, -79.58552",
+          "new": "43.74020, -79.58552",
+          "arrow": "\u2193 7.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7033927,
+      "address_full": "470 Prince Edward Dr N",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.65675,
+      "longitude": -79.5094,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.65682, -79.50926",
+          "new": "43.65675, -79.50940",
+          "arrow": "\u2199 13.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 982435,
+      "address_full": "1515 Albion Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.73997,
+      "longitude": -79.58482,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73985, -79.58417",
+          "new": "43.73997, -79.58482",
+          "arrow": "\u2190 54.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30059338,
+      "address_full": "65 Underhill Dr",
+      "municipality_name": "North York",
+      "latitude": 43.74593,
+      "longitude": -79.32285,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.74538, -79.32485",
+          "new": "43.74593, -79.32285",
+          "arrow": "\u2192 172.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7033924,
+      "address_full": "465 Prince Edward Dr N",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.6566,
+      "longitude": -79.50844,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.65661, -79.50852",
+          "new": "43.65660, -79.50844",
+          "arrow": "\u2192 6.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 2218905,
+      "address_full": "10 Denny Crt",
+      "municipality_name": "Scarborough",
+      "latitude": 43.75429,
+      "longitude": -79.17857,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75427, -79.17870",
+          "new": "43.75429, -79.17857",
+          "arrow": "\u2192 10.7m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (latitude)": 7,
+      "Location (longitude)": 6
+    }
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 7
+}
