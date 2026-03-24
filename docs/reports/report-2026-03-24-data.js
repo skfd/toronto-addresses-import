4a367aa60393d88b64be362aa05e82b854102200
skfd <kk@comentality.com>
2026-03-24 13:00:56 -0400

    data 2026-03-24

---
+++ docs/reports/report-2026-03-24-data.js
@@ -0,0 +1,90 @@
+window.REPORT_DATA = {
+  "generated": "Mar 24, 2026 at 01:00 PM",
+  "old_snapshot": {
+    "id": 18,
+    "downloaded": "2026-03-23T13:00:10.252581",
+    "row_count": 525379,
+    "filename": "address-points-2026-03-23.geojson",
+    "remote_last_modified": "Mon, 23 Mar 2026 11:40:28 GMT",
+    "remote_content_length": 589181602,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 19,
+    "downloaded": "2026-03-24T13:00:08.449133",
+    "row_count": 525379,
+    "filename": "address-points-2026-03-24.geojson",
+    "remote_last_modified": "Tue, 24 Mar 2026 11:39:04 GMT",
+    "remote_content_length": 589181637,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, Mar 23, 2026",
+  "new_date_friendly": "Tuesday, Mar 24, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 7650394,
+      "address_full": "1867 Lake Shore Blvd E",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66506,
+      "longitude": -79.30467,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66510, -79.30491",
+          "new": "43.66506, -79.30467",
+          "arrow": "\u2192 19.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14078430,
+      "address_full": "10 Judson St",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.61627,
+      "longitude": -79.499,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.61623, -79.49937",
+          "new": "43.61627, -79.49900",
+          "arrow": "\u2192 30.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7777417,
+      "address_full": "115 Felstead Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.67737,
+      "longitude": -79.32566,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67881, -79.32610",
+          "new": "43.67737, -79.32566",
+          "arrow": "\u2193 164.2m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 3,
+      "Location (latitude)": 3
+    }
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 3
+}
