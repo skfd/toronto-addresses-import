c92452083c33c21d17cc65e1a62b52704e43cda5
skfd <kk@comentality.com>
2026-03-25 18:39:32 -0400

    data 2026-03-25

---
+++ docs/reports/report-2026-03-25-data.js
@@ -0,0 +1,335 @@
+window.REPORT_DATA = {
+  "generated": "Mar 25, 2026 at 06:39 PM",
+  "old_snapshot": {
+    "id": 19,
+    "downloaded": "2026-03-24T13:00:08.449133",
+    "row_count": 525379,
+    "filename": "address-points-2026-03-24.geojson",
+    "remote_last_modified": "Tue, 24 Mar 2026 11:39:04 GMT",
+    "remote_content_length": 589181637,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 20,
+    "downloaded": "2026-03-25T18:38:19.007138",
+    "row_count": 525381,
+    "filename": "address-points-2026-03-25.geojson",
+    "remote_last_modified": "Wed, 25 Mar 2026 11:38:15 GMT",
+    "remote_content_length": 589183903,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Mar 24, 2026",
+  "new_date_friendly": "Wednesday, Mar 25, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 20,
+      "max_snapshot_id": 20,
+      "address_point_id": 60066705,
+      "address_full": "476 Front St E",
+      "address_number": "476",
+      "lo_num": 476,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Front St E",
+      "linear_name": "Front",
+      "linear_name_type": "St",
+      "linear_name_dir": "E",
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto Centre",
+      "longitude": -79.35514,
+      "latitude": 43.65345,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007541, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60003648, \"CENTRELINE_ID\": 30043802, \"CENTRELINE_MEASURE\": 63.88, \"CENTRELINE_OFFSET\": 21.82, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3471.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051807\", \"PLACE_NAME\": \"Diamond Jubilee Promenade\", \"PLACE_NAME_ALL\": \"Diamond Jubilee Promenade\", \"WARD\": \"13\"}"
+    },
+    {
+      "min_snapshot_id": 20,
+      "max_snapshot_id": 20,
+      "address_point_id": 60066701,
+      "address_full": "1085A Davenport Rd",
+      "address_number": "1085A",
+      "lo_num": 1085,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Davenport Rd",
+      "linear_name": "Davenport",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.42806,
+      "latitude": 43.6746,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007529, \"ADDRESS_ID_LINK\": 131186, \"ADDRESS_POINT_ID_LINK\": 8007482, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006259, \"CENTRELINE_ID\": 8007458, \"CENTRELINE_MEASURE\": 148.17, \"CENTRELINE_OFFSET\": 16.13, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3269.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051808\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    },
+    {
+      "min_snapshot_id": 20,
+      "max_snapshot_id": 20,
+      "address_point_id": 60066702,
+      "address_full": "1241 Shaw St",
+      "address_number": "1241",
+      "lo_num": 1241,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shaw St",
+      "linear_name": "Shaw",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.42804,
+      "latitude": 43.67445,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007531, \"ADDRESS_ID_LINK\": 131186, \"ADDRESS_POINT_ID_LINK\": 8007482, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006260, \"CENTRELINE_ID\": 14004027, \"CENTRELINE_MEASURE\": 0.82, \"CENTRELINE_OFFSET\": 11.14, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4388.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051809\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    },
+    {
+      "min_snapshot_id": 20,
+      "max_snapshot_id": 20,
+      "address_point_id": 60066703,
+      "address_full": "1243 Shaw St",
+      "address_number": "1243",
+      "lo_num": 1243,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shaw St",
+      "linear_name": "Shaw",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.42805,
+      "latitude": 43.67447,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007533, \"ADDRESS_ID_LINK\": 131186, \"ADDRESS_POINT_ID_LINK\": 8007482, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006261, \"CENTRELINE_ID\": 14004027, \"CENTRELINE_MEASURE\": 2.98, \"CENTRELINE_OFFSET\": 11.13, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4388.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051810\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}"
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 19,
+      "address_point_id": 982433,
+      "address_full": "1513 Albion Rd",
+      "address_number": "1513",
+      "lo_num": 1513,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Albion Rd",
+      "linear_name": "Albion",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke North",
+      "longitude": -79.58381,
+      "latitude": 43.7398,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 76568, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 614163, \"CENTRELINE_ID\": 906946, \"CENTRELINE_MEASURE\": 276.63, \"CENTRELINE_OFFSET\": 48.24, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1012.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"620368\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"01\"}"
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 19,
+      "address_point_id": 575667,
+      "address_full": "232 Willowdale Ave",
+      "address_number": "232",
+      "lo_num": 232,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Willowdale Ave",
+      "linear_name": "Willowdale",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Willowdale",
+      "longitude": -79.40248,
+      "latitude": 43.76985,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 418928, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 435267, \"CENTRELINE_ID\": 438829, \"CENTRELINE_MEASURE\": 18.74, \"CENTRELINE_OFFSET\": 24.91, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7093.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"1036308\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"18\"}"
+    }
+  ],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 373328,
+      "address_full": "1247 Pharmacy Ave",
+      "municipality_name": "Scarborough",
+      "latitude": 43.74965,
+      "longitude": -79.30683,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.74963, -79.30669",
+          "new": "43.74965, -79.30683",
+          "arrow": "\u2190 11.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 11272812,
+      "address_full": "175 Scarlett Rd",
+      "municipality_name": "York",
+      "latitude": 43.67485,
+      "longitude": -79.49768,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67396, -79.50446",
+          "new": "43.67485, -79.49768",
+          "arrow": "\u2192 554.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8007915,
+      "address_full": "1087 Davenport Rd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.67459,
+      "longitude": -79.42853,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67463, -79.42858",
+          "new": "43.67459, -79.42853",
+          "arrow": "\u2198 6.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30035744,
+      "address_full": "32 Lisgar St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64159,
+      "longitude": -79.42312,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64154, -79.42326",
+          "new": "43.64159, -79.42312",
+          "arrow": "\u2197 12.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 373327,
+      "address_full": "1248 Pharmacy Ave",
+      "municipality_name": "Scarborough",
+      "latitude": 43.7496,
+      "longitude": -79.30762,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.74984, -79.30760",
+          "new": "43.74960, -79.30762",
+          "arrow": "\u2193 26.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8007407,
+      "address_full": "1237 Shaw St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.67435,
+      "longitude": -79.42793,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67437, -79.42792",
+          "new": "43.67435, -79.42793",
+          "arrow": "\u2193 2.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 3331421,
+      "address_full": "55 Eltham Dr",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.60282,
+      "longitude": -79.54816,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.60300, -79.54815",
+          "new": "43.60282, -79.54816",
+          "arrow": "\u2193 20.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8007408,
+      "address_full": "1239 Shaw St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.67439,
+      "longitude": -79.42796,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67441, -79.42794",
+          "new": "43.67439, -79.42796",
+          "arrow": "\u2199 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 20003953,
+      "address_full": "75 Gihon Spring Dr",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.75548,
+      "longitude": -79.59487,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75571, -79.59479",
+          "new": "43.75548, -79.59487",
+          "arrow": "\u2193 26.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8007482,
+      "address_full": "1085 Davenport Rd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6746,
+      "longitude": -79.4281,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67457, -79.42806",
+          "new": "43.67460, -79.42810",
+          "arrow": "\u2196 4.6m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 4
+    },
+    "muni_removed": {
+      "Etobicoke": 1,
+      "North York": 1
+    },
+    "ward_added": {
+      "Toronto-St. Paul's": 3,
+      "Toronto Centre": 1
+    },
+    "ward_removed": {
+      "Etobicoke North": 1,
+      "Willowdale": 1
+    },
+    "field_changes": {
+      "Location (longitude)": 10,
+      "Location (latitude)": 10
+    }
+  },
+  "added_count": 4,
+  "removed_count": 2,
+  "modified_count": 0,
+  "modified_location_count": 10
+}
