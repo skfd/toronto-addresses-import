aa7162b6625f435e7b600f0d5dd6e7cf6cf34077
skfd <kk@comentality.com>
2026-03-27 18:13:10 -0400

    data 2026-03-27

---
+++ docs/reports/report-2026-03-27-data.js
@@ -0,0 +1,58 @@
+window.REPORT_DATA = {
+  "generated": "Mar 27, 2026 at 06:13 PM",
+  "old_snapshot": {
+    "id": 21,
+    "downloaded": "2026-03-27T01:23:07.465181",
+    "row_count": 525387,
+    "filename": "address-points-2026-03-26.geojson",
+    "remote_last_modified": "Thu, 26 Mar 2026 11:39:16 GMT",
+    "remote_content_length": 589190643,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 22,
+    "downloaded": "2026-03-27T18:11:37.514930",
+    "row_count": 525387,
+    "filename": "address-points-2026-03-27.geojson",
+    "remote_last_modified": "Fri, 27 Mar 2026 11:38:29 GMT",
+    "remote_content_length": 589190620,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, Mar 27, 2026",
+  "new_date_friendly": "Friday, Mar 27, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 20232266,
+      "address_full": "2530 Birchmount Rd",
+      "municipality_name": "Scarborough",
+      "latitude": 43.78767,
+      "longitude": -79.30166,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.78768, -79.30213",
+          "new": "43.78767, -79.30166",
+          "arrow": "\u2192 37.8m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 1,
+      "Location (latitude)": 1
+    }
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 1
+}
