af68ad3802dce641826d6f68c38371172fe690b7
skfd <kk@comentality.com>
2026-03-27 13:32:21 -0400

    data 2026-03-27

---
+++ docs/reports/report-2026-03-26-data.js
@@ -0,0 +1,247 @@
+window.REPORT_DATA = {
+  "generated": "Mar 27, 2026 at 01:32 PM",
+  "old_snapshot": {
+    "id": 20,
+    "downloaded": "2026-03-25T18:38:19.007138",
+    "row_count": 525381,
+    "filename": "address-points-2026-03-25.geojson",
+    "remote_last_modified": "Wed, 25 Mar 2026 11:38:15 GMT",
+    "remote_content_length": 589183903,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 21,
+    "downloaded": "2026-03-27T01:23:07.465181",
+    "row_count": 525387,
+    "filename": "address-points-2026-03-26.geojson",
+    "remote_last_modified": "Thu, 26 Mar 2026 11:39:16 GMT",
+    "remote_content_length": 589190643,
+    "skipped": 0
+  },
+  "old_date_friendly": "Wednesday, Mar 25, 2026",
+  "new_date_friendly": "Friday, Mar 27, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 21,
+      "max_snapshot_id": 21,
+      "address_point_id": 60067024,
+      "address_full": "6E Shorncliffe Rd",
+      "address_number": "6E",
+      "lo_num": 6,
+      "lo_num_suf": "E",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shorncliffe Rd",
+      "linear_name": "Shorncliffe",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54462,
+      "latitude": 43.63146,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007560, \"ADDRESS_ID_LINK\": 60007549, \"ADDRESS_POINT_ID_LINK\": 60067018, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006266, \"CENTRELINE_ID\": 912916, \"CENTRELINE_MEASURE\": 68.36, \"CENTRELINE_OFFSET\": 28.06, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2478.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051811\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 21,
+      "max_snapshot_id": 21,
+      "address_point_id": 60067022,
+      "address_full": "6C Shorncliffe Rd",
+      "address_number": "6C",
+      "lo_num": 6,
+      "lo_num_suf": "C",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shorncliffe Rd",
+      "linear_name": "Shorncliffe",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54466,
+      "latitude": 43.63154,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007556, \"ADDRESS_ID_LINK\": 60007549, \"ADDRESS_POINT_ID_LINK\": 60067018, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006264, \"CENTRELINE_ID\": 912916, \"CENTRELINE_MEASURE\": 58.84, \"CENTRELINE_OFFSET\": 27.03, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2478.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051812\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 21,
+      "max_snapshot_id": 21,
+      "address_point_id": 60067023,
+      "address_full": "6D Shorncliffe Rd",
+      "address_number": "6D",
+      "lo_num": 6,
+      "lo_num_suf": "D",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shorncliffe Rd",
+      "linear_name": "Shorncliffe",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54464,
+      "latitude": 43.63151,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007558, \"ADDRESS_ID_LINK\": 60007549, \"ADDRESS_POINT_ID_LINK\": 60067018, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006265, \"CENTRELINE_ID\": 912916, \"CENTRELINE_MEASURE\": 63.08, \"CENTRELINE_OFFSET\": 27.38, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2478.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051813\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 21,
+      "max_snapshot_id": 21,
+      "address_point_id": 60067018,
+      "address_full": "5507 Dundas St W",
+      "address_number": "5507",
+      "lo_num": 5507,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Dundas St W",
+      "linear_name": "Dundas",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54477,
+      "latitude": 43.63176,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007549, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 9396, \"CENTRELINE_ID\": 912884, \"CENTRELINE_MEASURE\": 31.66, \"CENTRELINE_OFFSET\": 30.59, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 558.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051814\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 21,
+      "max_snapshot_id": 21,
+      "address_point_id": 60067021,
+      "address_full": "6B Shorncliffe Rd",
+      "address_number": "6B",
+      "lo_num": 6,
+      "lo_num_suf": "B",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shorncliffe Rd",
+      "linear_name": "Shorncliffe",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54469,
+      "latitude": 43.6316,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007554, \"ADDRESS_ID_LINK\": 60007549, \"ADDRESS_POINT_ID_LINK\": 60067018, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006263, \"CENTRELINE_ID\": 912916, \"CENTRELINE_MEASURE\": 52.13, \"CENTRELINE_OFFSET\": 25.81, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2478.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051815\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    },
+    {
+      "min_snapshot_id": 21,
+      "max_snapshot_id": 21,
+      "address_point_id": 60067020,
+      "address_full": "6A Shorncliffe Rd",
+      "address_number": "6A",
+      "lo_num": 6,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Shorncliffe Rd",
+      "linear_name": "Shorncliffe",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54472,
+      "latitude": 43.63167,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007552, \"ADDRESS_ID_LINK\": 60007549, \"ADDRESS_POINT_ID_LINK\": 60067018, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006262, \"CENTRELINE_ID\": 912916, \"CENTRELINE_MEASURE\": 47.49, \"CENTRELINE_OFFSET\": 24.84, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2478.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051816\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}"
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 300379,
+      "address_full": "58 Adanac Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.73806,
+      "longitude": -79.23085,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73806, -79.23086",
+          "new": "43.73806, -79.23085",
+          "arrow": "\u2192 0.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 490714,
+      "address_full": "50 Blue Ridge Rd",
+      "municipality_name": "North York",
+      "latitude": 43.77338,
+      "longitude": -79.37533,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.77334, -79.37531",
+          "new": "43.77338, -79.37533",
+          "arrow": "\u2191 4.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10618790,
+      "address_full": "1603 The Queensway",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.6168,
+      "longitude": -79.54321,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.61661, -79.54309",
+          "new": "43.61680, -79.54321",
+          "arrow": "\u2196 23.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10618804,
+      "address_full": "1631 The Queensway",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.61596,
+      "longitude": -79.54522,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.61614, -79.54517",
+          "new": "43.61596, -79.54522",
+          "arrow": "\u2193 20.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10618789,
+      "address_full": "1601 The Queensway",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.61682,
+      "longitude": -79.54315,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.61695, -79.54295",
+          "new": "43.61682, -79.54315",
+          "arrow": "\u2199 21.7m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "Etobicoke": 6
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Etobicoke-Lakeshore": 6
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 5,
+      "Location (latitude)": 4
+    }
+  },
+  "added_count": 6,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 5
+}
