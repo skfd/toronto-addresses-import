cbc2f74ec0e4bc76d66b87afd855a68133e842ed
skfd <kk@comentality.com>
2026-04-08 13:38:52 -0400

    data 2026-04-08

---
+++ docs/reports/report-2026-04-08-data.js
@@ -0,0 +1,63 @@
+window.REPORT_DATA = {
+  "generated": "Apr 08, 2026 at 01:38 PM",
+  "old_snapshot": {
+    "id": 24,
+    "downloaded": "2026-04-03T13:00:08.881855",
+    "row_count": 525388,
+    "filename": "address-points-2026-04-03.geojson",
+    "remote_last_modified": "Fri, 03 Apr 2026 11:38:20 GMT",
+    "remote_content_length": 589191846,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 25,
+    "downloaded": "2026-04-08T13:35:54.768955",
+    "row_count": 525389,
+    "filename": "address-points-2026-04-08.geojson",
+    "remote_last_modified": "Wed, 08 Apr 2026 11:39:40 GMT",
+    "remote_content_length": 589192940,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, Apr 03, 2026",
+  "new_date_friendly": "Wednesday, Apr 08, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 25,
+      "max_snapshot_id": 25,
+      "address_point_id": 60068080,
+      "address_full": "89 Avenue Rd",
+      "address_number": "89",
+      "lo_num": 89,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Avenue Rd",
+      "linear_name": "Avenue",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "University-Rosedale",
+      "longitude": -79.39532,
+      "latitude": 43.67235,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007652, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 126007, \"CENTRELINE_ID\": 30126052, \"CENTRELINE_MEASURE\": 46.41, \"CENTRELINE_OFFSET\": 22.78, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2924.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051836\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"11\"}"
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 1
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "University-Rosedale": 1
+    },
+    "ward_removed": {},
+    "field_changes": {}
+  },
+  "added_count": 1,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0
+}
