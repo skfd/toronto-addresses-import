97f6f44f49b4b6e95c8aaf60c8e9edb0a2a99cb0
skfd <kk@comentality.com>
2026-04-10 16:08:31 -0400

    data 2026-04-10

---
+++ docs/reports/report-2026-04-10-data.js
@@ -0,0 +1,287 @@
+window.REPORT_DATA = {
+  "generated": "Apr 10, 2026 at 04:08 PM",
+  "old_snapshot": {
+    "id": 25,
+    "downloaded": "2026-04-08T13:35:54.768955",
+    "row_count": 525389,
+    "filename": "address-points-2026-04-08.geojson",
+    "remote_last_modified": "Thu, 09 Apr 2026 11:40:01 GMT",
+    "remote_content_length": 589192912,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 26,
+    "downloaded": "2026-04-10T16:07:14.829339",
+    "row_count": 525393,
+    "filename": "address-points-2026-04-10.geojson",
+    "remote_last_modified": "Fri, 10 Apr 2026 11:36:36 GMT",
+    "remote_content_length": 589197536,
+    "skipped": 0
+  },
+  "old_date_friendly": "Wednesday, Apr 08, 2026",
+  "new_date_friendly": "Friday, Apr 10, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 26,
+      "max_snapshot_id": 26,
+      "address_point_id": 60068084,
+      "address_full": "5933 Steeles Ave E",
+      "address_number": "5933",
+      "lo_num": 5933,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Steeles Ave E",
+      "linear_name": "Steeles",
+      "linear_name_type": "Ave",
+      "linear_name_dir": "E",
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough North",
+      "longitude": -79.25855,
+      "latitude": 43.83461,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007658, \"ADDRESS_ID_LINK\": 60007655, \"ADDRESS_POINT_ID_LINK\": 60068083, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006280, \"CENTRELINE_ID\": 14303971, \"CENTRELINE_MEASURE\": 209.94, \"CENTRELINE_OFFSET\": 30.17, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 6792.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051837\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"23\"}"
+    },
+    {
+      "min_snapshot_id": 26,
+      "max_snapshot_id": 26,
+      "address_point_id": 60068085,
+      "address_full": "5951 Steeles Ave E",
+      "address_number": "5951",
+      "lo_num": 5951,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Steeles Ave E",
+      "linear_name": "Steeles",
+      "linear_name_type": "Ave",
+      "linear_name_dir": "E",
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough North",
+      "longitude": -79.25819,
+      "latitude": 43.83472,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007660, \"ADDRESS_ID_LINK\": 60007655, \"ADDRESS_POINT_ID_LINK\": 60068083, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 508311, \"CENTRELINE_ID\": 14303971, \"CENTRELINE_MEASURE\": 240.51, \"CENTRELINE_OFFSET\": 27.67, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 6792.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051838\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"23\"}"
+    },
+    {
+      "min_snapshot_id": 26,
+      "max_snapshot_id": 26,
+      "address_point_id": 60068087,
+      "address_full": "105 State Crown Blvd",
+      "address_number": "105",
+      "lo_num": 105,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "State Crown Blvd",
+      "linear_name": "State Crown",
+      "linear_name_type": "Blvd",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough North",
+      "longitude": -79.25973,
+      "latitude": 43.83247,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007662, \"ADDRESS_ID_LINK\": 60007655, \"ADDRESS_POINT_ID_LINK\": 60068083, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006281, \"CENTRELINE_ID\": 60046363, \"CENTRELINE_MEASURE\": 52.35, \"CENTRELINE_OFFSET\": 45.36, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 9004.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051839\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"23\"}"
+    },
+    {
+      "min_snapshot_id": 26,
+      "max_snapshot_id": 26,
+      "address_point_id": 60068083,
+      "address_full": "5855 Steeles Ave E",
+      "address_number": "5855",
+      "lo_num": 5855,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Steeles Ave E",
+      "linear_name": "Steeles",
+      "linear_name_type": "Ave",
+      "linear_name_dir": "E",
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough North",
+      "longitude": -79.26049,
+      "latitude": 43.83403,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007655, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 616679, \"CENTRELINE_ID\": 14303971, \"CENTRELINE_MEASURE\": 41.61, \"CENTRELINE_OFFSET\": 44.54, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 6792.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051840\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"23\"}"
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 7641552,
+      "address_full": "36A Florence Cres",
+      "municipality_name": "York",
+      "latitude": 43.66937,
+      "longitude": -79.49469,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66938, -79.49476",
+          "new": "43.66937, -79.49469",
+          "arrow": "\u2192 5.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9085981,
+      "address_full": "7 Eileen Ave",
+      "municipality_name": "York",
+      "latitude": 43.66946,
+      "longitude": -79.49534,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66933, -79.49537",
+          "new": "43.66946, -79.49534",
+          "arrow": "\u2191 14.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560565,
+      "address_full": "689 Seneca Hill Dr",
+      "municipality_name": "North York",
+      "latitude": 43.79336,
+      "longitude": -79.35006,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.79338, -79.35003",
+          "new": "43.79336, -79.35006",
+          "arrow": "\u2199 3.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14676556,
+      "address_full": "45 Country Lane",
+      "municipality_name": "North York",
+      "latitude": 43.74126,
+      "longitude": -79.3762,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.74130, -79.37628",
+          "new": "43.74126, -79.37620",
+          "arrow": "\u2198 7.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9668254,
+      "address_full": "99 Prue Ave",
+      "municipality_name": "North York",
+      "latitude": 43.70774,
+      "longitude": -79.436,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70764, -79.43571",
+          "new": "43.70774, -79.43600",
+          "arrow": "\u2196 25.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560566,
+      "address_full": "691 Seneca Hill Dr",
+      "municipality_name": "North York",
+      "latitude": 43.79342,
+      "longitude": -79.35005,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.79345, -79.35012",
+          "new": "43.79342, -79.35005",
+          "arrow": "\u2198 6.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 982432,
+      "address_full": "1485 Albion Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.73934,
+      "longitude": -79.58118,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73928, -79.58119",
+          "new": "43.73934, -79.58118",
+          "arrow": "\u2191 6.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 987997,
+      "address_full": "290 Birmingham St",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.60037,
+      "longitude": -79.5207,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.60067, -79.52057",
+          "new": "43.60037, -79.52070",
+          "arrow": "\u2193 35.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 536511,
+      "address_full": "160 Ledbury St",
+      "municipality_name": "North York",
+      "latitude": 43.72714,
+      "longitude": -79.42494,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72771, -79.42433",
+          "new": "43.72714, -79.42494",
+          "arrow": "\u2199 80.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 523758,
+      "address_full": "150 Grenoble Dr",
+      "municipality_name": "North York",
+      "latitude": 43.71354,
+      "longitude": -79.32651,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.71353, -79.32660",
+          "new": "43.71354, -79.32651",
+          "arrow": "\u2192 7.3m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "Scarborough": 4
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Scarborough North": 4
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 10,
+      "Location (latitude)": 10
+    }
+  },
+  "added_count": 4,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 10
+}
