930966a3e79f5c93203846954ff9f33485929159
skfd <kk@comentality.com>
2026-04-17 22:47:28 -0400

    data 2026-04-17

---
+++ docs/reports/report-2026-04-16-data.js
@@ -0,0 +1,301 @@
+window.REPORT_DATA = {
+  "generated": "Apr 17, 2026 at 10:47 PM",
+  "old_snapshot": {
+    "id": 27,
+    "downloaded": "2026-04-13T13:00:05.584921",
+    "row_count": 525401,
+    "filename": "address-points-2026-04-13.geojson",
+    "remote_last_modified": "Mon, 13 Apr 2026 11:36:48 GMT",
+    "remote_content_length": 589206558,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 28,
+    "downloaded": "2026-04-17T22:46:21.627053",
+    "row_count": 525404,
+    "filename": "address-points-2026-04-16.geojson",
+    "remote_last_modified": "Thu, 16 Apr 2026 11:37:01 GMT",
+    "remote_content_length": 589209924,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, Apr 13, 2026",
+  "new_date_friendly": "Friday, Apr 17, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 28,
+      "max_snapshot_id": 28,
+      "address_point_id": 60068938,
+      "address_full": "9 Don River Blvd",
+      "address_number": "9",
+      "lo_num": 9,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Don River Blvd",
+      "linear_name": "Don River",
+      "linear_name_type": "Blvd",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "York Centre",
+      "longitude": -79.43041,
+      "latitude": 43.7568,
+      "extra": "{\"ADDRESS_CLASS\": \"LE\", \"ADDRESS_CLASS_DESC\": \"Land Entrance\", \"ADDRESS_ID\": 60007693, \"ADDRESS_ID_LINK\": 478321, \"ADDRESS_POINT_ID_LINK\": 9246594, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006289, \"CENTRELINE_ID\": 30035892, \"CENTRELINE_MEASURE\": 215.26, \"CENTRELINE_OFFSET\": 29.36, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10100, \"CLASS_FAMILY_DESC\": \"Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 5402.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"1051849\", \"PLACE_NAME\": \"Parking Lot - Earl Bales Park\", \"PLACE_NAME_ALL\": \"Parking Lot - Earl Bales Park\", \"WARD\": \"06\"}"
+    },
+    {
+      "min_snapshot_id": 28,
+      "max_snapshot_id": 28,
+      "address_point_id": 60068939,
+      "address_full": "127 Glenlake Ave",
+      "address_number": "127",
+      "lo_num": 127,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Glenlake Ave",
+      "linear_name": "Glenlake",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Parkdale-High Park",
+      "longitude": -79.45968,
+      "latitude": 43.65847,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007694, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 61028, \"CENTRELINE_ID\": 14013850, \"CENTRELINE_MEASURE\": 5.7, \"CENTRELINE_OFFSET\": 18.21, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3546.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051850\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"04\"}"
+    },
+    {
+      "min_snapshot_id": 28,
+      "max_snapshot_id": 28,
+      "address_point_id": 60068940,
+      "address_full": "129 Glenlake Ave",
+      "address_number": "129",
+      "lo_num": 129,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Glenlake Ave",
+      "linear_name": "Glenlake",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Parkdale-High Park",
+      "longitude": -79.45974,
+      "latitude": 43.65846,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007695, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60004893, \"CENTRELINE_ID\": 14013850, \"CENTRELINE_MEASURE\": 10.73, \"CENTRELINE_OFFSET\": 17.94, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3546.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051851\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"04\"}"
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 515268,
+      "address_full": "108 Fenside Dr",
+      "municipality_name": "North York",
+      "latitude": 43.76302,
+      "longitude": -79.32902,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.76299, -79.32909",
+          "new": "43.76302, -79.32902",
+          "arrow": "\u2197 6.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 563085,
+      "address_full": "28 Slidell Cres",
+      "municipality_name": "North York",
+      "latitude": 43.76386,
+      "longitude": -79.32748,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.76387, -79.32753",
+          "new": "43.76386, -79.32748",
+          "arrow": "\u2192 4.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9246594,
+      "address_full": "4169 Bathurst St",
+      "municipality_name": "North York",
+      "latitude": 43.75451,
+      "longitude": -79.43392,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75258, -79.43188",
+          "new": "43.75451, -79.43392",
+          "arrow": "\u2196 270.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10923450,
+      "address_full": "495 Sherbourne St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6678,
+      "longitude": -79.37463,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66780, -79.37448",
+          "new": "43.66780, -79.37463",
+          "arrow": "\u2190 12.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 13969580,
+      "address_full": "50 Keywest Ave",
+      "municipality_name": "York",
+      "latitude": 43.69286,
+      "longitude": -79.44842,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.69295, -79.44843",
+          "new": "43.69286, -79.44842",
+          "arrow": "\u2193 10.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 20082204,
+      "address_full": "80 Thyra Ave",
+      "municipality_name": "East York",
+      "latitude": 43.69414,
+      "longitude": -79.29279,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.69399, -79.29243",
+          "new": "43.69414, -79.29279",
+          "arrow": "\u2196 33.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 563069,
+      "address_full": "2 Slidell Cres",
+      "municipality_name": "North York",
+      "latitude": 43.76358,
+      "longitude": -79.32895,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.76363, -79.32893",
+          "new": "43.76358, -79.32895",
+          "arrow": "\u2193 5.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9085699,
+      "address_full": "4100 Dundas St W",
+      "municipality_name": "York",
+      "latitude": 43.66497,
+      "longitude": -79.50601,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66344, -79.50537",
+          "new": "43.66497, -79.50601",
+          "arrow": "\u2191 177.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 574231,
+      "address_full": "3100 Weston Rd",
+      "municipality_name": "North York",
+      "latitude": 43.73387,
+      "longitude": -79.53894,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73370, -79.53962",
+          "new": "43.73387, -79.53894",
+          "arrow": "\u2192 57.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 6739479,
+      "address_full": "2 Roywood Dr",
+      "municipality_name": "North York",
+      "latitude": 43.7638,
+      "longitude": -79.32174,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.76385, -79.32173",
+          "new": "43.76380, -79.32174",
+          "arrow": "\u2193 5.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 563087,
+      "address_full": "30 Slidell Cres",
+      "municipality_name": "North York",
+      "latitude": 43.76437,
+      "longitude": -79.32696,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.76384, -79.32682",
+          "new": "43.76437, -79.32696",
+          "arrow": "\u2191 60.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8039130,
+      "address_full": "35 Glen Long Ave",
+      "municipality_name": "North York",
+      "latitude": 43.70878,
+      "longitude": -79.45587,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70863, -79.45580",
+          "new": "43.70878, -79.45587",
+          "arrow": "\u2191 17.6m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 2,
+      "North York": 1
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Parkdale-High Park": 2,
+      "York Centre": 1
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 12,
+      "Location (latitude)": 11
+    }
+  },
+  "added_count": 3,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 12
+}
