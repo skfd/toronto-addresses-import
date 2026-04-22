d94116d8237ecc42604001dad6c3a281e01ad79f
skfd <kk@comentality.com>
2026-04-22 15:28:04 -0400

    data 2026-04-22

---
+++ docs/reports/report-2026-04-22-data.js
@@ -0,0 +1,147 @@
+window.REPORT_DATA = {
+  "generated": "Apr 22, 2026 at 03:28 PM",
+  "old_snapshot": {
+    "id": 29,
+    "downloaded": "2026-04-21T22:09:23.346756",
+    "row_count": 525404,
+    "filename": "address-points-2026-04-21.geojson",
+    "remote_last_modified": "Tue, 21 Apr 2026 11:37:11 GMT",
+    "remote_content_length": 589209950,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 30,
+    "downloaded": "2026-04-22T15:26:02.458149",
+    "row_count": 525404,
+    "filename": "address-points-2026-04-22.geojson",
+    "remote_last_modified": "Wed, 22 Apr 2026 11:37:14 GMT",
+    "remote_content_length": 589209950,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Apr 21, 2026",
+  "new_date_friendly": "Wednesday, Apr 22, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 13973135,
+      "address_full": "24 Rockwood Dr",
+      "municipality_name": "Scarborough",
+      "latitude": 43.72889,
+      "longitude": -79.2309,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.72959, -79.23011",
+          "new": "43.72889, -79.23090",
+          "arrow": "\u2199 100.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9085362,
+      "address_full": "275 Bathurst St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.65121,
+      "longitude": -79.40446,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.65110, -79.40504",
+          "new": "43.65121, -79.40446",
+          "arrow": "\u2192 48.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10363749,
+      "address_full": "1260 Dufferin St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66736,
+      "longitude": -79.43922,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66732, -79.43927",
+          "new": "43.66736, -79.43922",
+          "arrow": "\u2197 6.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10034958,
+      "address_full": "610 Broadview Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.67244,
+      "longitude": -79.35711,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67250, -79.35612",
+          "new": "43.67244, -79.35711",
+          "arrow": "\u2190 80.0m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 4,
+      "Location (latitude)": 4
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 4,
+  "sparklines": {
+    "added": [
+      0,
+      3,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      12,
+      0,
+      0,
+      0,
+      10,
+      0
+    ],
+    "modified_location": [
+      0,
+      12,
+      0,
+      0,
+      0,
+      10,
+      4
+    ]
+  }
+}
