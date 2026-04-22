e1c963c570860f23d575063f09f94b2de43a30da
skfd <kk@comentality.com>
2026-04-21 23:03:28 -0400

    data 2026-04-21

---
+++ docs/reports/report-2026-04-21-data.js
@@ -0,0 +1,202 @@
+window.REPORT_DATA = {
+  "generated": "Apr 21, 2026 at 11:03 PM",
+  "old_snapshot": {
+    "id": 28,
+    "downloaded": "2026-04-17T22:46:21.627053",
+    "row_count": 525404,
+    "filename": "address-points-2026-04-16.geojson",
+    "remote_last_modified": "Thu, 16 Apr 2026 11:37:01 GMT",
+    "remote_content_length": 589209924,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 29,
+    "downloaded": "2026-04-21T22:09:23.346756",
+    "row_count": 525404,
+    "filename": "address-points-2026-04-21.geojson",
+    "remote_last_modified": "Tue, 21 Apr 2026 11:37:11 GMT",
+    "remote_content_length": 589209950,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, Apr 17, 2026",
+  "new_date_friendly": "Tuesday, Apr 21, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 14171223,
+      "address_full": "530 Progress Ave",
+      "municipality_name": "Scarborough",
+      "latitude": 43.7792,
+      "longitude": -79.25657,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.77897, -79.25560",
+          "new": "43.77920, -79.25657",
+          "arrow": "\u2190 82.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7795922,
+      "address_full": "150 Greenwood Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66964,
+      "longitude": -79.32817,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66948, -79.32922",
+          "new": "43.66964, -79.32817",
+          "arrow": "\u2192 86.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 12383210,
+      "address_full": "89 Church Ave",
+      "municipality_name": "North York",
+      "latitude": 43.77474,
+      "longitude": -79.40849,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.77464, -79.40854",
+          "new": "43.77474, -79.40849",
+          "arrow": "\u2191 11.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10034904,
+      "address_full": "953 Gerrard St E",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66793,
+      "longitude": -79.33997,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66805, -79.33940",
+          "new": "43.66793, -79.33997",
+          "arrow": "\u2190 47.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 537896,
+      "address_full": "45 Logandale Rd",
+      "municipality_name": "North York",
+      "latitude": 43.77691,
+      "longitude": -79.40962,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.77691, -79.40970",
+          "new": "43.77691, -79.40962",
+          "arrow": "\u2192 6.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 9909587,
+      "address_full": "582 Yonge St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66517,
+      "longitude": -79.38493,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66519, -79.38492",
+          "new": "43.66517, -79.38493",
+          "arrow": "\u2193 2.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 563174,
+      "address_full": "184 Sloane Ave",
+      "municipality_name": "North York",
+      "latitude": 43.736,
+      "longitude": -79.3129,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.73597, -79.31289",
+          "new": "43.73600, -79.31290",
+          "arrow": "\u2191 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 762469,
+      "address_full": "61 Alton Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66557,
+      "longitude": -79.32825,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66556, -79.32825",
+          "new": "43.66557, -79.32825",
+          "arrow": "\u2191 1.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30074689,
+      "address_full": "209 Alton Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6695,
+      "longitude": -79.32969,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66976, -79.32970",
+          "new": "43.66950, -79.32969",
+          "arrow": "\u2193 29.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 11272812,
+      "address_full": "175 Scarlett Rd",
+      "municipality_name": "York",
+      "latitude": 43.67484,
+      "longitude": -79.49765,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.67485, -79.49768",
+          "new": "43.67484, -79.49765",
+          "arrow": "\u2198 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 9,
+      "Location (latitude)": 9
+    }
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 10
+}
