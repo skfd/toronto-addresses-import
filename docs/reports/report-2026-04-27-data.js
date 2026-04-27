af9b01cf16948d56158f230246ffbdca58045765
skfd <kk@comentality.com>
2026-04-27 19:21:07 -0400

    data 2026-04-27

---
+++ docs/reports/report-2026-04-27-data.js
@@ -0,0 +1,340 @@
+window.REPORT_DATA = {
+  "generated": "Apr 27, 2026 at 07:21 PM",
+  "old_snapshot": {
+    "id": 32,
+    "downloaded": "2026-04-25T18:29:50.379305",
+    "row_count": 525410,
+    "filename": "address-points-2026-04-24.geojson",
+    "remote_last_modified": "Fri, 24 Apr 2026 11:37:51 GMT",
+    "remote_content_length": 589216654,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 33,
+    "downloaded": "2026-04-27T19:20:20.598064",
+    "row_count": 525412,
+    "filename": "address-points-2026-04-27.geojson",
+    "remote_last_modified": "Mon, 27 Apr 2026 11:35:58 GMT",
+    "remote_content_length": 589218920,
+    "skipped": 0
+  },
+  "old_date_friendly": "Saturday, Apr 25, 2026",
+  "new_date_friendly": "Monday, Apr 27, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 33,
+      "max_snapshot_id": 33,
+      "address_point_id": 60069591,
+      "address_full": "623R Broadview Ave",
+      "address_number": "623R",
+      "lo_num": 623,
+      "lo_num_suf": "R",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Broadview Ave",
+      "linear_name": "Broadview",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-Danforth",
+      "longitude": -79.35503,
+      "latitude": 43.6728,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007903, \"ADDRESS_ID_LINK\": 253605, \"ADDRESS_POINT_ID_LINK\": 10034968, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006296, \"CENTRELINE_ID\": 10034966, \"CENTRELINE_MEASURE\": 9.69, \"CENTRELINE_OFFSET\": 41.24, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 44.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051860\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"14\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 33,
+      "max_snapshot_id": 33,
+      "address_point_id": 60069592,
+      "address_full": "407R Arlington Ave",
+      "address_number": "407R",
+      "lo_num": 407,
+      "lo_num_suf": "R",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Arlington Ave",
+      "linear_name": "Arlington",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "York",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.43185,
+      "latitude": 43.68981,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007905, \"ADDRESS_ID_LINK\": 94131, \"ADDRESS_POINT_ID_LINK\": 50578, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006297, \"CENTRELINE_ID\": 14067860, \"CENTRELINE_MEASURE\": 43.56, \"CENTRELINE_OFFSET\": 41.33, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 412.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"YK\", \"OBJECTID\": \"1051861\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 7553374,
+      "address_full": "1179 King St W",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63922,
+      "longitude": -79.42522,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63911, -79.42519",
+          "new": "43.63922, -79.42522",
+          "arrow": "\u2191 12.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10142966,
+      "address_full": "763 Royal York Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.63734,
+      "longitude": -79.50655,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63729, -79.50685",
+          "new": "43.63734, -79.50655",
+          "arrow": "\u2192 24.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 20029600,
+      "address_full": "120 The Queensway",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.63338,
+      "longitude": -79.47631,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63290, -79.47711",
+          "new": "43.63338, -79.47631",
+          "arrow": "\u2197 83.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 13990645,
+      "address_full": "76 Fraser Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63867,
+      "longitude": -79.42475,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63836, -79.42468",
+          "new": "43.63867, -79.42475",
+          "arrow": "\u2191 35.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 2297674,
+      "address_full": "1 North Kingslea Dr",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.64143,
+      "longitude": -79.50051,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64150, -79.50031",
+          "new": "43.64143, -79.50051",
+          "arrow": "\u2199 17.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30113850,
+      "address_full": "69 Fraser Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63849,
+      "longitude": -79.42371,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63836, -79.42415",
+          "new": "43.63849, -79.42371",
+          "arrow": "\u2192 38.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 10143004,
+      "address_full": "767 Royal York Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.63774,
+      "longitude": -79.50673,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63768, -79.50708",
+          "new": "43.63774, -79.50673",
+          "arrow": "\u2192 29.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 20028204,
+      "address_full": "340 Park Lawn Rd",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.63891,
+      "longitude": -79.49569,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63858, -79.49567",
+          "new": "43.63891, -79.49569",
+          "arrow": "\u2191 36.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 13990646,
+      "address_full": "74 Fraser Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63853,
+      "longitude": -79.42469,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63827, -79.42465",
+          "new": "43.63853, -79.42469",
+          "arrow": "\u2191 29.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14063181,
+      "address_full": "74A Fraser Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63859,
+      "longitude": -79.42473,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63830, -79.42479",
+          "new": "43.63859, -79.42473",
+          "arrow": "\u2191 32.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 20029597,
+      "address_full": "75 High St",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.63436,
+      "longitude": -79.48426,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63121, -79.48251",
+          "new": "43.63436, -79.48426",
+          "arrow": "\u2191 377.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 3132236,
+      "address_full": "290 The West Mall",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.6346,
+      "longitude": -79.56214,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63465, -79.56180",
+          "new": "43.63460, -79.56214",
+          "arrow": "\u2190 28.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 988556,
+      "address_full": "4201 Bloor St W",
+      "municipality_name": "Etobicoke",
+      "latitude": 43.63518,
+      "longitude": -79.56336,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63526, -79.56288",
+          "new": "43.63518, -79.56336",
+          "arrow": "\u2190 39.7m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 1,
+      "York": 1
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Toronto-Danforth": 1,
+      "Toronto-St. Paul's": 1
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 13,
+      "Location (latitude)": 13
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 2,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 13,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      8,
+      0,
+      0,
+      2
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      2,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      10,
+      4,
+      64,
+      54,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      10,
+      4,
+      64,
+      54,
+      0,
+      13
+    ]
+  }
+}
