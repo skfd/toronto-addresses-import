eddc42130c8c2042d231d8368d6d7ae1a92da7e2
skfd <kk@comentality.com>
2026-04-28 13:02:05 -0400

    data 2026-04-28

---
+++ docs/reports/report-2026-04-28-data.js
@@ -0,0 +1,202 @@
+window.REPORT_DATA = {
+  "generated": "Apr 28, 2026 at 01:02 PM",
+  "old_snapshot": {
+    "id": 33,
+    "downloaded": "2026-04-27T19:20:20.598064",
+    "row_count": 525412,
+    "filename": "address-points-2026-04-27.geojson",
+    "remote_last_modified": "Mon, 27 Apr 2026 11:35:58 GMT",
+    "remote_content_length": 589218920,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 34,
+    "downloaded": "2026-04-28T13:00:44.179622",
+    "row_count": 525415,
+    "filename": "address-points-2026-04-28.geojson",
+    "remote_last_modified": "Tue, 28 Apr 2026 11:36:36 GMT",
+    "remote_content_length": 589222240,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, Apr 27, 2026",
+  "new_date_friendly": "Tuesday, Apr 28, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 34,
+      "max_snapshot_id": 34,
+      "address_point_id": 60069721,
+      "address_full": "88 Isabella St",
+      "address_number": "88",
+      "lo_num": 88,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Isabella St",
+      "linear_name": "Isabella",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto Centre",
+      "longitude": -79.38131,
+      "latitude": 43.66877,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007943, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 159166, \"CENTRELINE_ID\": 20079096, \"CENTRELINE_MEASURE\": 20.54, \"CENTRELINE_OFFSET\": 29.64, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3734.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051862\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"13\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 34,
+      "max_snapshot_id": 34,
+      "address_point_id": 60069723,
+      "address_full": "251 Staines Rd",
+      "address_number": "251",
+      "lo_num": 251,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Staines Rd",
+      "linear_name": "Staines",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough-Rouge Park",
+      "longitude": -79.22141,
+      "latitude": 43.82798,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007945, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60001835, \"CENTRELINE_ID\": 60007828, \"CENTRELINE_MEASURE\": 79.44, \"CENTRELINE_OFFSET\": 88.09, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 8994.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051863\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"25\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 34,
+      "max_snapshot_id": 34,
+      "address_point_id": 60069722,
+      "address_full": "77 Clarendon Ave",
+      "address_number": "77",
+      "lo_num": 77,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Clarendon Ave",
+      "linear_name": "Clarendon",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.40601,
+      "latitude": 43.6823,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007944, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 129137, \"CENTRELINE_ID\": 1139997, \"CENTRELINE_MEASURE\": 81.95, \"CENTRELINE_OFFSET\": 34.31, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3174.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051864\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 20078708,
+      "address_full": "70 East Liberty St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63876,
+      "longitude": -79.41446,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63884, -79.41475",
+          "new": "43.63876, -79.41446",
+          "arrow": "\u2192 25.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14121184,
+      "address_full": "1155 King St W",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63911,
+      "longitude": -79.42258,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63902, -79.42323",
+          "new": "43.63911, -79.42258",
+          "arrow": "\u2192 53.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30113850,
+      "address_full": "69 Fraser Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.63787,
+      "longitude": -79.42366,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.63849, -79.42371",
+          "new": "43.63787, -79.42366",
+          "arrow": "\u2193 69.1m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 2,
+      "Scarborough": 1
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Toronto Centre": 1,
+      "Scarborough-Rouge Park": 1,
+      "Toronto-St. Paul's": 1
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Location (longitude)": 3,
+      "Location (latitude)": 3
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 3,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 3,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      8,
+      0,
+      0,
+      2,
+      3
+    ],
+    "removed": [
+      0,
+      0,
+      2,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      10,
+      4,
+      64,
+      54,
+      0,
+      13,
+      0
+    ],
+    "modified_location": [
+      10,
+      4,
+      64,
+      54,
+      0,
+      13,
+      3
+    ]
+  }
+}
