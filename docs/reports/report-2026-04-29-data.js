e2e3449128c9e75982a8ba39933766a98ef52af7
skfd <kk@comentality.com>
2026-04-29 14:34:24 -0400

    data 2026-04-29

---
+++ docs/reports/report-2026-04-29-data.js
@@ -0,0 +1,315 @@
+window.REPORT_DATA = {
+  "generated": "Apr 29, 2026 at 02:34 PM",
+  "old_snapshot": {
+    "id": 34,
+    "downloaded": "2026-04-28T13:00:44.179622",
+    "row_count": 525415,
+    "filename": "address-points-2026-04-28.geojson",
+    "remote_last_modified": "Tue, 28 Apr 2026 11:36:36 GMT",
+    "remote_content_length": 589222240,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 35,
+    "downloaded": "2026-04-29T14:32:50.091243",
+    "row_count": 525416,
+    "filename": "address-points-2026-04-29.geojson",
+    "remote_last_modified": "Wed, 29 Apr 2026 11:33:51 GMT",
+    "remote_content_length": 589223415,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Apr 28, 2026",
+  "new_date_friendly": "Wednesday, Apr 29, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 35,
+      "max_snapshot_id": 35,
+      "address_point_id": 60069724,
+      "address_full": "17 Kingsley Ave",
+      "address_number": "17",
+      "lo_num": 17,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Kingsley Ave",
+      "linear_name": "Kingsley",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Davenport",
+      "longitude": -79.45287,
+      "latitude": 43.66821,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60007951, \"ADDRESS_ID_LINK\": 1618851, \"ADDRESS_POINT_ID_LINK\": 30157966, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 63274, \"CENTRELINE_ID\": 14013297, \"CENTRELINE_MEASURE\": 0.82, \"CENTRELINE_OFFSET\": 18.01, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3783.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051865\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"09\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 35,
+      "max_snapshot_id": 35,
+      "address_point_id": 60069725,
+      "address_full": "400 William R Allen Rd",
+      "address_number": "400",
+      "lo_num": 400,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "William R Allen Rd",
+      "linear_name": "William R Allen Rd",
+      "linear_name_type": null,
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "York Centre",
+      "longitude": -79.46051,
+      "latitude": 43.74625,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60007953, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006298, \"CENTRELINE_ID\": 60007071, \"CENTRELINE_MEASURE\": 710.26, \"CENTRELINE_OFFSET\": 47.07, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 11008.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"1051866\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"06\"}",
+      "history": []
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 34,
+      "address_point_id": 7939953,
+      "address_full": "52 Cannon Rd",
+      "address_number": "52",
+      "lo_num": 52,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Cannon Rd",
+      "linear_name": "Cannon",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.48927,
+      "latitude": 43.63391,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 180101, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 188570, \"CENTRELINE_ID\": 60000275, \"CENTRELINE_MEASURE\": 275.68, \"CENTRELINE_OFFSET\": 24.9, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1291.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"698399\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-04-29",
+          "kind": "removed",
+          "snapshot_id": 35,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 560119,
+      "address_full": "64 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75799,
+      "longitude": -79.5689,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75796, -79.56892",
+          "new": "43.75799, -79.56890",
+          "arrow": "\u2197 3.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8492685,
+      "address_full": "42 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75714,
+      "longitude": -79.56855,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75716, -79.56861",
+          "new": "43.75714, -79.56855",
+          "arrow": "\u2198 5.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560125,
+      "address_full": "70 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75824,
+      "longitude": -79.56901,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75826, -79.56907",
+          "new": "43.75824, -79.56901",
+          "arrow": "\u2198 5.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8492687,
+      "address_full": "46 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75729,
+      "longitude": -79.56863,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75731, -79.56869",
+          "new": "43.75729, -79.56863",
+          "arrow": "\u2198 5.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560123,
+      "address_full": "68 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75814,
+      "longitude": -79.56897,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75811, -79.56901",
+          "new": "43.75814, -79.56897",
+          "arrow": "\u2197 4.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8492690,
+      "address_full": "52 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75751,
+      "longitude": -79.56872,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75748, -79.56873",
+          "new": "43.75751, -79.56872",
+          "arrow": "\u2191 3.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560109,
+      "address_full": "54 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75761,
+      "longitude": -79.56876,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75762, -79.56883",
+          "new": "43.75761, -79.56876",
+          "arrow": "\u2192 5.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560115,
+      "address_full": "60 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75783,
+      "longitude": -79.56885,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75779, -79.56889",
+          "new": "43.75783, -79.56885",
+          "arrow": "\u2197 5.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 560129,
+      "address_full": "74 Seacliff Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.7584,
+      "longitude": -79.5691,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.75842, -79.56914",
+          "new": "43.75840, -79.56910",
+          "arrow": "\u2198 3.9m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 1,
+      "North York": 1
+    },
+    "muni_removed": {
+      "Etobicoke": 1
+    },
+    "ward_added": {
+      "Davenport": 1,
+      "York Centre": 1
+    },
+    "ward_removed": {
+      "Etobicoke-Lakeshore": 1
+    },
+    "field_changes": {
+      "Location (longitude)": 9,
+      "Location (latitude)": 9
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 2,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 9,
+  "sparklines": {
+    "added": [
+      0,
+      8,
+      0,
+      0,
+      2,
+      3,
+      2
+    ],
+    "removed": [
+      0,
+      2,
+      0,
+      0,
+      0,
+      0,
+      1
+    ],
+    "modified": [
+      4,
+      64,
+      54,
+      0,
+      13,
+      3,
+      0
+    ],
+    "modified_location": [
+      4,
+      64,
+      54,
+      0,
+      13,
+      3,
+      9
+    ]
+  }
+}
