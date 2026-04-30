a11598088df2bbe75e2b2bc55b76afe3fa012a8e
skfd <kk@comentality.com>
2026-04-30 15:28:54 -0400

    data 2026-04-30

---
+++ docs/reports/report-2026-04-30-data.js
@@ -0,0 +1,627 @@
+window.REPORT_DATA = {
+  "generated": "Apr 30, 2026 at 03:28 PM",
+  "old_snapshot": {
+    "id": 35,
+    "downloaded": "2026-04-29T14:32:50.091243",
+    "row_count": 525416,
+    "filename": "address-points-2026-04-29.geojson",
+    "remote_last_modified": "Wed, 29 Apr 2026 11:33:51 GMT",
+    "remote_content_length": 589223415,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 36,
+    "downloaded": "2026-04-30T15:26:03.111137",
+    "row_count": 525416,
+    "filename": "address-points-2026-04-30.geojson",
+    "remote_last_modified": "Thu, 30 Apr 2026 11:39:08 GMT",
+    "remote_content_length": 589223387,
+    "skipped": 0
+  },
+  "old_date_friendly": "Wednesday, Apr 29, 2026",
+  "new_date_friendly": "Thursday, Apr 30, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 14207205,
+      "address_full": "33 Scarboro Beach Blvd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66942,
+      "longitude": -79.29244,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66945, -79.29231",
+          "new": "43.66942, -79.29244",
+          "arrow": "\u2190 11.0m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074940,
+      "address_full": "42 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6472,
+      "longitude": -79.36972,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64747, -79.36980",
+          "new": "43.64720, -79.36972",
+          "arrow": "\u2193 30.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 6168209,
+      "address_full": "32 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6474,
+      "longitude": -79.3698,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64730, -79.36974",
+          "new": "43.64740, -79.36980",
+          "arrow": "\u2196 12.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30071225,
+      "address_full": "32 Wellesley St E",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66543,
+      "longitude": -79.383,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66552, -79.38296",
+          "new": "43.66543, -79.38300",
+          "arrow": "\u2193 10.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14207204,
+      "address_full": "37 Scarboro Beach Blvd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66953,
+      "longitude": -79.29251,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66959, -79.29234",
+          "new": "43.66953, -79.29251",
+          "arrow": "\u2199 15.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074934,
+      "address_full": "28 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64747,
+      "longitude": -79.36983,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64724, -79.36971",
+          "new": "43.64747, -79.36983",
+          "arrow": "\u2191 27.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14205185,
+      "address_full": "18 George St S",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64749,
+      "longitude": -79.36946,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64743, -79.36945",
+          "new": "43.64749, -79.36946",
+          "arrow": "\u2191 6.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 865208,
+      "address_full": "35 Scarboro Beach Blvd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66948,
+      "longitude": -79.29247,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66953, -79.29246",
+          "new": "43.66948, -79.29247",
+          "arrow": "\u2193 5.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074939,
+      "address_full": "40 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64724,
+      "longitude": -79.36973,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64744, -79.36978",
+          "new": "43.64724, -79.36973",
+          "arrow": "\u2193 22.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 6168210,
+      "address_full": "52 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64719,
+      "longitude": -79.36938,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64763, -79.36955",
+          "new": "43.64719, -79.36938",
+          "arrow": "\u2193 50.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14200481,
+      "address_full": "32 Glen Manor Dr",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66941,
+      "longitude": -79.29192,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66944, -79.29198",
+          "new": "43.66941, -79.29192",
+          "arrow": "\u2198 5.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074941,
+      "address_full": "44 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64716,
+      "longitude": -79.3697,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64750, -79.36981",
+          "new": "43.64716, -79.36970",
+          "arrow": "\u2193 38.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074942,
+      "address_full": "46 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64712,
+      "longitude": -79.36968,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64754, -79.36982",
+          "new": "43.64712, -79.36968",
+          "arrow": "\u2193 48.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 865207,
+      "address_full": "31 Scarboro Beach Blvd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66936,
+      "longitude": -79.29241,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66940, -79.29242",
+          "new": "43.66936, -79.29241",
+          "arrow": "\u2193 4.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14208836,
+      "address_full": "34 Glen Manor Dr",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66945,
+      "longitude": -79.29194,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66945, -79.29205",
+          "new": "43.66945, -79.29194",
+          "arrow": "\u2192 8.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30113912,
+      "address_full": "34 Wellesley St E",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66544,
+      "longitude": -79.38293,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66554, -79.38290",
+          "new": "43.66544, -79.38293",
+          "arrow": "\u2193 11.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074932,
+      "address_full": "24 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64753,
+      "longitude": -79.36986,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64718, -79.36969",
+          "new": "43.64753, -79.36986",
+          "arrow": "\u2191 41.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074938,
+      "address_full": "38 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64727,
+      "longitude": -79.36974,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64740, -79.36977",
+          "new": "43.64727, -79.36974",
+          "arrow": "\u2193 14.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8351461,
+      "address_full": "26 George St S",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64763,
+      "longitude": -79.36952,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64756, -79.36951",
+          "new": "43.64763, -79.36952",
+          "arrow": "\u2191 7.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 870439,
+      "address_full": "475 Spadina Rd",
+      "municipality_name": "former Toronto",
+      "latitude": 43.69085,
+      "longitude": -79.41323,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.69089, -79.41323",
+          "new": "43.69085, -79.41323",
+          "arrow": "\u2193 4.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14205184,
+      "address_full": "20 George St S",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64752,
+      "longitude": -79.36947,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64746, -79.36947",
+          "new": "43.64752, -79.36947",
+          "arrow": "\u2191 6.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074931,
+      "address_full": "22 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64755,
+      "longitude": -79.36987,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64716, -79.36967",
+          "new": "43.64755, -79.36987",
+          "arrow": "\u2191 46.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14205183,
+      "address_full": "22 George St S",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64756,
+      "longitude": -79.36949,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64749, -79.36948",
+          "new": "43.64756, -79.36949",
+          "arrow": "\u2191 7.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074933,
+      "address_full": "26 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6475,
+      "longitude": -79.36984,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64721, -79.36970",
+          "new": "43.64750, -79.36984",
+          "arrow": "\u2191 34.2m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8351455,
+      "address_full": "133 Wilton St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64808,
+      "longitude": -79.36887,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64810, -79.36897",
+          "new": "43.64808, -79.36887",
+          "arrow": "\u2192 8.4m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074937,
+      "address_full": "36 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64731,
+      "longitude": -79.36976,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64738, -79.36976",
+          "new": "43.64731, -79.36976",
+          "arrow": "\u2193 7.8m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074936,
+      "address_full": "34 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64734,
+      "longitude": -79.36978,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64735, -79.36975",
+          "new": "43.64734, -79.36978",
+          "arrow": "\u2199 2.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074930,
+      "address_full": "20 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64757,
+      "longitude": -79.36982,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64713, -79.36966",
+          "new": "43.64757, -79.36982",
+          "arrow": "\u2191 50.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30071224,
+      "address_full": "28 Wellesley St E",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66542,
+      "longitude": -79.38308,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.66551, -79.38302",
+          "new": "43.66542, -79.38308",
+          "arrow": "\u2199 11.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074943,
+      "address_full": "48 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64712,
+      "longitude": -79.36964,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64756, -79.36983",
+          "new": "43.64712, -79.36964",
+          "arrow": "\u2193 51.3m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14074935,
+      "address_full": "30 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64744,
+      "longitude": -79.36982,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64727, -79.36972",
+          "new": "43.64744, -79.36982",
+          "arrow": "\u2196 20.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 6168194,
+      "address_full": "32A Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64737,
+      "longitude": -79.36979,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64733, -79.36975",
+          "new": "43.64737, -79.36979",
+          "arrow": "\u2196 5.5m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14205182,
+      "address_full": "24 George St S",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6476,
+      "longitude": -79.36951,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64752, -79.36949",
+          "new": "43.64760, -79.36951",
+          "arrow": "\u2191 9.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 6168208,
+      "address_full": "2 Henry Lane Ter",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64763,
+      "longitude": -79.36956,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.64719, -79.36935",
+          "new": "43.64763, -79.36956",
+          "arrow": "\u2191 51.8m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Location (latitude)": 33,
+      "Location (longitude)": 31
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 34,
+  "sparklines": {
+    "added": [
+      8,
+      0,
+      0,
+      2,
+      3,
+      2,
+      0
+    ],
+    "removed": [
+      2,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0
+    ],
+    "modified": [
+      64,
+      54,
+      0,
+      13,
+      3,
+      9,
+      0
+    ],
+    "modified_location": [
+      64,
+      54,
+      0,
+      13,
+      3,
+      9,
+      34
+    ]
+  }
+}
