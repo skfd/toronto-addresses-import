6d59c8d66aab89b658934e6f0ca73d60259eeee2
skfd <kk@comentality.com>
2026-05-04 13:01:04 -0400

    data 2026-05-04

---
+++ docs/reports/report-2026-05-04-data.js
@@ -0,0 +1,118 @@
+window.REPORT_DATA = {
+  "generated": "May 04, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 37,
+    "downloaded": "2026-05-01T13:00:38.959164",
+    "row_count": 525414,
+    "filename": "address-points-2026-05-01.geojson",
+    "remote_last_modified": "Fri, 01 May 2026 11:37:50 GMT",
+    "remote_content_length": 589221128,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 38,
+    "downloaded": "2026-05-04T13:00:14.071207",
+    "row_count": 525413,
+    "filename": "address-points-2026-05-04.geojson",
+    "remote_last_modified": "Mon, 04 May 2026 11:37:57 GMT",
+    "remote_content_length": 589220015,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, May 01, 2026",
+  "new_date_friendly": "Monday, May 04, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 37,
+      "address_point_id": 30033468,
+      "address_full": "47 Foxbar Rd",
+      "address_number": "47",
+      "lo_num": 47,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Foxbar Rd",
+      "linear_name": "Foxbar",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.39908,
+      "latitude": 43.68603,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1519398, \"ADDRESS_ID_LINK\": 1512782, \"ADDRESS_POINT_ID_LINK\": 30029365, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 613073, \"CENTRELINE_ID\": 30017340, \"CENTRELINE_MEASURE\": 228.5, \"CENTRELINE_OFFSET\": 22.42, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3459.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"666171\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-04",
+          "kind": "removed",
+          "snapshot_id": 38,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "former Toronto": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "Toronto-St. Paul's": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      3,
+      2,
+      0,
+      1,
+      0,
+      0,
+      0
+    ],
+    "removed": [
+      0,
+      1,
+      0,
+      3,
+      0,
+      0,
+      1
+    ],
+    "modified": [
+      3,
+      9,
+      34,
+      98,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      3,
+      9,
+      34,
+      98,
+      0,
+      0,
+      0
+    ]
+  }
+}
