82bc5187ca244e744e59dbc4597aa651659547dd
skfd <kk@comentality.com>
2026-05-08 22:46:03 -0400

    data 2026-05-08

---
+++ docs/reports/report-2026-05-08-data.js
@@ -0,0 +1,188 @@
+window.REPORT_DATA = {
+  "generated": "May 08, 2026 at 10:46 PM",
+  "old_snapshot": {
+    "id": 39,
+    "downloaded": "2026-05-07T13:00:10.490523",
+    "row_count": 525412,
+    "filename": "address-points-2026-05-07.geojson",
+    "remote_last_modified": "Thu, 07 May 2026 11:43:13 GMT",
+    "remote_content_length": 589218401,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 40,
+    "downloaded": "2026-05-08T21:04:19.901012",
+    "row_count": 525414,
+    "filename": "address-points-2026-05-08.geojson",
+    "remote_last_modified": "Fri, 08 May 2026 11:41:48 GMT",
+    "remote_content_length": 589220256,
+    "skipped": 0
+  },
+  "old_date_friendly": "Thursday, May 07, 2026",
+  "new_date_friendly": "Friday, May 08, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 40,
+      "max_snapshot_id": 40,
+      "address_point_id": 60069901,
+      "address_full": "33B Bexhill Ave",
+      "address_number": "33B",
+      "lo_num": 33,
+      "lo_num_suf": "B",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bexhill Ave",
+      "linear_name": "Bexhill",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.28835,
+      "latitude": 43.70206,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008294, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006310, \"CENTRELINE_ID\": 112692, \"CENTRELINE_MEASURE\": 87.86, \"CENTRELINE_OFFSET\": 25.75, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7357.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051874\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 40,
+      "max_snapshot_id": 40,
+      "address_point_id": 60069866,
+      "address_full": "6 Rusholme Rd",
+      "address_number": "6",
+      "lo_num": 6,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Rusholme Rd",
+      "linear_name": "Rusholme",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Davenport",
+      "longitude": -79.427,
+      "latitude": 43.64977,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008273, \"ADDRESS_ID_LINK\": 162671, \"ADDRESS_POINT_ID_LINK\": 7548718, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006308, \"CENTRELINE_ID\": 14020415, \"CENTRELINE_MEASURE\": 34.11, \"CENTRELINE_OFFSET\": 10.21, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4340.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051875\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"09\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 40,
+      "max_snapshot_id": 40,
+      "address_point_id": 60069900,
+      "address_full": "33A Bexhill Ave",
+      "address_number": "33A",
+      "lo_num": 33,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bexhill Ave",
+      "linear_name": "Bexhill",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.28832,
+      "latitude": 43.702,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008291, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006309, \"CENTRELINE_ID\": 112692, \"CENTRELINE_MEASURE\": 81.15, \"CENTRELINE_OFFSET\": 26.11, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7357.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051876\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": []
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 39,
+      "address_point_id": 14206991,
+      "address_full": "2514 Bloor St W",
+      "address_number": "2514",
+      "lo_num": 2514,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bloor St W",
+      "linear_name": "Bloor",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "former Toronto",
+      "ward_name": "Parkdale-High Park",
+      "longitude": -79.48892,
+      "latitude": 43.648,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1478248, \"ADDRESS_ID_LINK\": 658681, \"ADDRESS_POINT_ID_LINK\": 877443, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 184788, \"CENTRELINE_ID\": 11272600, \"CENTRELINE_MEASURE\": 14.45, \"CENTRELINE_OFFSET\": 26.24, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 458.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"571096\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"04\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-08",
+          "kind": "removed",
+          "snapshot_id": 40,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Scarborough": 2,
+      "former Toronto": 1
+    },
+    "muni_removed": {
+      "former Toronto": 1
+    },
+    "ward_added": {
+      "Scarborough Southwest": 2,
+      "Davenport": 1
+    },
+    "ward_removed": {
+      "Parkdale-High Park": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 3,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      1,
+      0,
+      0,
+      0,
+      0,
+      6,
+      3
+    ],
+    "removed": [
+      3,
+      0,
+      0,
+      1,
+      0,
+      7,
+      1
+    ],
+    "modified": [
+      98,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      98,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
