c04c87d91c30f1ffeb94f7689bad2c9319250a49
skfd <kk@comentality.com>
2026-05-11 13:01:25 -0400

    data 2026-05-11

---
+++ docs/reports/report-2026-05-11-data.js
@@ -0,0 +1,118 @@
+window.REPORT_DATA = {
+  "generated": "May 11, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 40,
+    "downloaded": "2026-05-08T21:04:19.901012",
+    "row_count": 525414,
+    "filename": "address-points-2026-05-08.geojson",
+    "remote_last_modified": "Fri, 08 May 2026 11:41:48 GMT",
+    "remote_content_length": 589220256,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 41,
+    "downloaded": "2026-05-11T13:00:12.495461",
+    "row_count": 525413,
+    "filename": "address-points-2026-05-11.geojson",
+    "remote_last_modified": "Mon, 11 May 2026 11:42:25 GMT",
+    "remote_content_length": 589218605,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, May 08, 2026",
+  "new_date_friendly": "Monday, May 11, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 40,
+      "address_point_id": 31794,
+      "address_full": "45 Peard Rd",
+      "address_number": "45",
+      "lo_num": 45,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Peard Rd",
+      "linear_name": "Peard",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "East York",
+      "ward_name": "Beaches-East York",
+      "longitude": -79.29921,
+      "latitude": 43.70995,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 195165, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 209116, \"CENTRELINE_ID\": 378, \"CENTRELINE_MEASURE\": 183.79, \"CENTRELINE_OFFSET\": 20.08, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 288.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"EY\", \"OBJECTID\": \"902044\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"19\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-11",
+          "kind": "removed",
+          "snapshot_id": 41,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "East York": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "Beaches-East York": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      6,
+      3,
+      0,
+      0,
+      0
+    ],
+    "removed": [
+      1,
+      0,
+      7,
+      1,
+      0,
+      0,
+      1
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
