9f17605e4ffeb85d7714f320e7a5af40451404c6
skfd <kk@comentality.com>
2026-05-14 05:40:00 -0400

    data 2026-05-14

---
+++ docs/reports/report-2026-05-13-data.js
@@ -0,0 +1,79 @@
+window.REPORT_DATA = {
+  "generated": "May 14, 2026 at 05:40 AM",
+  "old_snapshot": {
+    "id": 42,
+    "downloaded": "2026-05-12T13:00:12.805671",
+    "row_count": 525412,
+    "filename": "address-points-2026-05-12.geojson",
+    "remote_last_modified": "Tue, 12 May 2026 11:41:24 GMT",
+    "remote_content_length": 589217055,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 43,
+    "downloaded": "2026-05-14T05:39:05.876426",
+    "row_count": 525412,
+    "filename": "address-points-2026-05-13.geojson",
+    "remote_last_modified": "Wed, 13 May 2026 11:39:33 GMT",
+    "remote_content_length": 589216854,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, May 12, 2026",
+  "new_date_friendly": "Thursday, May 14, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      6,
+      3,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "removed": [
+      7,
+      1,
+      0,
+      0,
+      1,
+      1,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
