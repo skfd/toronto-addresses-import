34b5009e078ce066bdf5e9d431d0fa935719572f
skfd <kk@comentality.com>
2026-05-15 17:11:32 -0400

    data 2026-05-15

---
+++ docs/reports/report-2026-05-15-data.js
@@ -0,0 +1,118 @@
+window.REPORT_DATA = {
+  "generated": "May 15, 2026 at 05:11 PM",
+  "old_snapshot": {
+    "id": 44,
+    "downloaded": "2026-05-14T13:00:11.165559",
+    "row_count": 525412,
+    "filename": "address-points-2026-05-14.geojson",
+    "remote_last_modified": "Thu, 14 May 2026 11:37:11 GMT",
+    "remote_content_length": 589216706,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 45,
+    "downloaded": "2026-05-15T17:09:52.069862",
+    "row_count": 525411,
+    "filename": "address-points-2026-05-15.geojson",
+    "remote_last_modified": "Fri, 15 May 2026 11:36:47 GMT",
+    "remote_content_length": 589215590,
+    "skipped": 0
+  },
+  "old_date_friendly": "Thursday, May 14, 2026",
+  "new_date_friendly": "Friday, May 15, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 44,
+      "address_point_id": 14205836,
+      "address_full": "189A Emerson Ave",
+      "address_number": "189A",
+      "lo_num": 189,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Emerson Ave",
+      "linear_name": "Emerson",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Davenport",
+      "longitude": -79.44295,
+      "latitude": 43.66605,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1474122, \"ADDRESS_ID_LINK\": 623447, \"ADDRESS_POINT_ID_LINK\": 10363749, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 145913, \"CENTRELINE_ID\": 30101046, \"CENTRELINE_MEASURE\": 20.43, \"CENTRELINE_OFFSET\": 59.06, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3396.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"922804\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"09\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-15",
+          "kind": "removed",
+          "snapshot_id": 45,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "former Toronto": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "Davenport": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "removed": [
+      0,
+      0,
+      1,
+      1,
+      0,
+      0,
+      1
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
