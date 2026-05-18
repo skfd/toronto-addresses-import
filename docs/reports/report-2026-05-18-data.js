a6f174199651e6583e8629aee702776889767641
skfd <kk@comentality.com>
2026-05-18 15:19:51 -0400

    data 2026-05-18

---
+++ docs/reports/report-2026-05-18-data.js
@@ -0,0 +1,148 @@
+window.REPORT_DATA = {
+  "generated": "May 18, 2026 at 03:19 PM",
+  "old_snapshot": {
+    "id": 45,
+    "downloaded": "2026-05-15T17:09:52.069862",
+    "row_count": 525411,
+    "filename": "address-points-2026-05-15.geojson",
+    "remote_last_modified": "Fri, 15 May 2026 11:36:47 GMT",
+    "remote_content_length": 589215590,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 46,
+    "downloaded": "2026-05-18T15:18:13.623344",
+    "row_count": 525414,
+    "filename": "address-points-2026-05-18.geojson",
+    "remote_last_modified": "Mon, 18 May 2026 11:41:16 GMT",
+    "remote_content_length": 589218371,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, May 15, 2026",
+  "new_date_friendly": "Monday, May 18, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 46,
+      "max_snapshot_id": 46,
+      "address_point_id": 60071501,
+      "address_full": "71 Lloyd Manor Rd",
+      "address_number": "71",
+      "lo_num": 71,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Lloyd Manor Rd",
+      "linear_name": "Lloyd Manor",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke Centre",
+      "longitude": -79.55053,
+      "latitude": 43.66485,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008471, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 43238, \"CENTRELINE_ID\": 910248, \"CENTRELINE_MEASURE\": 44.07, \"CENTRELINE_OFFSET\": 23.92, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2008.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051877\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"02\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 46,
+      "max_snapshot_id": 46,
+      "address_point_id": 60071500,
+      "address_full": "69 Lloyd Manor Rd",
+      "address_number": "69",
+      "lo_num": 69,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Lloyd Manor Rd",
+      "linear_name": "Lloyd Manor",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke Centre",
+      "longitude": -79.55035,
+      "latitude": 43.66472,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008468, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006312, \"CENTRELINE_ID\": 910248, \"CENTRELINE_MEASURE\": 23.2, \"CENTRELINE_OFFSET\": 24.56, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2008.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051878\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"02\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 46,
+      "max_snapshot_id": 46,
+      "address_point_id": 60071503,
+      "address_full": "457R Valermo Dr",
+      "address_number": "457R",
+      "lo_num": 457,
+      "lo_num_suf": "R",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Valermo Dr",
+      "linear_name": "Valermo",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54391,
+      "latitude": 43.60633,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008472, \"ADDRESS_ID_LINK\": 8555, \"ADDRESS_POINT_ID_LINK\": 1041078, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006313, \"CENTRELINE_ID\": 914212, \"CENTRELINE_MEASURE\": 30.14, \"CENTRELINE_OFFSET\": 45.7, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2695.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051879\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Etobicoke": 3
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Etobicoke Centre": 2,
+      "Etobicoke-Lakeshore": 1
+    },
+    "ward_removed": {},
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 3,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      3
+    ],
+    "removed": [
+      1,
+      0,
+      0,
+      1,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
