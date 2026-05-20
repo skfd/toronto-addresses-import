ae17d64879fa788c3932b8daad4a6056b7487129
skfd <kk@comentality.com>
2026-05-20 17:22:17 -0400

    data 2026-05-20

---
+++ docs/reports/report-2026-05-20-data.js
@@ -0,0 +1,220 @@
+window.REPORT_DATA = {
+  "generated": "May 20, 2026 at 05:22 PM",
+  "old_snapshot": {
+    "id": 46,
+    "downloaded": "2026-05-18T15:18:13.623344",
+    "row_count": 525414,
+    "filename": "address-points-2026-05-18.geojson",
+    "remote_last_modified": "Mon, 18 May 2026 11:41:16 GMT",
+    "remote_content_length": 589218371,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 47,
+    "downloaded": "2026-05-20T17:20:22.790784",
+    "row_count": 525415,
+    "filename": "address-points-2026-05-20.geojson",
+    "remote_last_modified": "Wed, 20 May 2026 11:39:44 GMT",
+    "remote_content_length": 589219241,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, May 18, 2026",
+  "new_date_friendly": "Wednesday, May 20, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 47,
+      "max_snapshot_id": 47,
+      "address_point_id": 60071551,
+      "address_full": "4918 Dundas St W",
+      "address_number": "4918",
+      "lo_num": 4918,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Dundas St W",
+      "linear_name": "Dundas",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.52925,
+      "latitude": 43.64853,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008514, \"ADDRESS_ID_LINK\": 60008511, \"ADDRESS_POINT_ID_LINK\": 60071548, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 623183, \"CENTRELINE_ID\": 10759621, \"CENTRELINE_MEASURE\": 44.26, \"CENTRELINE_OFFSET\": 19.6, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 558.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051880\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 47,
+      "max_snapshot_id": 47,
+      "address_point_id": 60071548,
+      "address_full": "4916 Dundas St W",
+      "address_number": "4916",
+      "lo_num": 4916,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Dundas St W",
+      "linear_name": "Dundas",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.52918,
+      "latitude": 43.64861,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008511, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 623182, \"CENTRELINE_ID\": 10759621, \"CENTRELINE_MEASURE\": 33.46, \"CENTRELINE_OFFSET\": 19.3, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 558.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051881\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 47,
+      "max_snapshot_id": 47,
+      "address_point_id": 60071552,
+      "address_full": "4920 Dundas St W",
+      "address_number": "4920",
+      "lo_num": 4920,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Dundas St W",
+      "linear_name": "Dundas",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.52936,
+      "latitude": 43.64842,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008516, \"ADDRESS_ID_LINK\": 60008511, \"ADDRESS_POINT_ID_LINK\": 60071548, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 623184, \"CENTRELINE_ID\": 10759621, \"CENTRELINE_MEASURE\": 59.32, \"CENTRELINE_OFFSET\": 19.75, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 558.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051882\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": []
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 46,
+      "address_point_id": 30062368,
+      "address_full": "4920 Dundas St W",
+      "address_number": "4920",
+      "lo_num": 4920,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Dundas St W",
+      "linear_name": "Dundas",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.52912,
+      "latitude": 43.64862,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 1538930, \"ADDRESS_ID_LINK\": 12384, \"ADDRESS_POINT_ID_LINK\": 10759622, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 623184, \"CENTRELINE_ID\": 10759621, \"CENTRELINE_MEASURE\": 30.06, \"CENTRELINE_OFFSET\": 15.82, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 558.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"745663\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-20",
+          "kind": "removed",
+          "snapshot_id": 47,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 46,
+      "address_point_id": 30062367,
+      "address_full": "4918 Dundas St W",
+      "address_number": "4918",
+      "lo_num": 4918,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Dundas St W",
+      "linear_name": "Dundas",
+      "linear_name_type": "St",
+      "linear_name_dir": "W",
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.52909,
+      "latitude": 43.64865,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 1538928, \"ADDRESS_ID_LINK\": 12384, \"ADDRESS_POINT_ID_LINK\": 10759622, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 623183, \"CENTRELINE_ID\": 10759621, \"CENTRELINE_MEASURE\": 26.19, \"CENTRELINE_OFFSET\": 15.78, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 558.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"870990\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-20",
+          "kind": "removed",
+          "snapshot_id": 47,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Etobicoke": 3
+    },
+    "muni_removed": {
+      "Etobicoke": 2
+    },
+    "ward_added": {
+      "Etobicoke-Lakeshore": 3
+    },
+    "ward_removed": {
+      "Etobicoke-Lakeshore": 2
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 3,
+  "removed_count": 2,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      3,
+      3
+    ],
+    "removed": [
+      0,
+      0,
+      1,
+      0,
+      0,
+      0,
+      2
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
