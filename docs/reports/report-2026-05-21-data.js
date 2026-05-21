cc39776614d0507ae8470683c9cc2d74b7a0ba35
skfd <kk@comentality.com>
2026-05-21 15:30:38 -0400

    data 2026-05-21

---
+++ docs/reports/report-2026-05-21-data.js
@@ -0,0 +1,189 @@
+window.REPORT_DATA = {
+  "generated": "May 21, 2026 at 03:30 PM",
+  "old_snapshot": {
+    "id": 47,
+    "downloaded": "2026-05-20T17:20:22.790784",
+    "row_count": 525415,
+    "filename": "address-points-2026-05-20.geojson",
+    "remote_last_modified": "Wed, 20 May 2026 11:39:44 GMT",
+    "remote_content_length": 589219241,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 48,
+    "downloaded": "2026-05-21T15:28:03.640002",
+    "row_count": 525412,
+    "filename": "address-points-2026-05-21.geojson",
+    "remote_last_modified": "Thu, 21 May 2026 11:38:48 GMT",
+    "remote_content_length": 589215927,
+    "skipped": 0
+  },
+  "old_date_friendly": "Wednesday, May 20, 2026",
+  "new_date_friendly": "Thursday, May 21, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 47,
+      "address_point_id": 30064445,
+      "address_full": "1401 Lawrence Ave W",
+      "address_number": "1401",
+      "lo_num": 1401,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Lawrence Ave W",
+      "linear_name": "Lawrence",
+      "linear_name_type": "Ave",
+      "linear_name_dir": "W",
+      "municipality_name": "North York",
+      "ward_name": "York South-Weston",
+      "longitude": -79.47642,
+      "latitude": 43.70895,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 1540388, \"ADDRESS_ID_LINK\": 83851, \"ADDRESS_POINT_ID_LINK\": 14073776, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 624123, \"CENTRELINE_ID\": 446206, \"CENTRELINE_MEASURE\": 170.69, \"CENTRELINE_OFFSET\": 38.25, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 732.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"609489\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"05\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-21",
+          "kind": "removed",
+          "snapshot_id": 48,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 47,
+      "address_point_id": 14209005,
+      "address_full": "269 Front St E",
+      "address_number": "269",
+      "lo_num": 269,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Front St E",
+      "linear_name": "Front",
+      "linear_name_type": "St",
+      "linear_name_dir": "E",
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto Centre",
+      "longitude": -79.36339,
+      "latitude": 43.65103,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1479173, \"ADDRESS_ID_LINK\": 670214, \"ADDRESS_POINT_ID_LINK\": 8354026, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 198979, \"CENTRELINE_ID\": 8354022, \"CENTRELINE_MEASURE\": 40.03, \"CENTRELINE_OFFSET\": 40.69, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3471.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1009140\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"13\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-21",
+          "kind": "removed",
+          "snapshot_id": 48,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 47,
+      "address_point_id": 14205275,
+      "address_full": "269 Main St",
+      "address_number": "269",
+      "lo_num": 269,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Main St",
+      "linear_name": "Main",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Beaches-East York",
+      "longitude": -79.30057,
+      "latitude": 43.68772,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 1461085, \"ADDRESS_ID_LINK\": 200273, \"ADDRESS_POINT_ID_LINK\": 8211499, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 215056, \"CENTRELINE_ID\": 30074090, \"CENTRELINE_MEASURE\": 162.88, \"CENTRELINE_OFFSET\": 81.25, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 234.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1033531\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"19\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-05-21",
+          "kind": "removed",
+          "snapshot_id": 48,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "former Toronto": 2,
+      "North York": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "York South-Weston": 1,
+      "Toronto Centre": 1,
+      "Beaches-East York": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 3,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      3,
+      3,
+      0
+    ],
+    "removed": [
+      0,
+      1,
+      0,
+      0,
+      0,
+      2,
+      3
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
