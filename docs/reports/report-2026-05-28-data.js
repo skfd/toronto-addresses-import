4366336f24f4a812c95e33e4de1e62ad1484e35d
skfd <kk@comentality.com>
2026-05-28 13:01:06 -0400

    data 2026-05-28

---
+++ docs/reports/report-2026-05-28-data.js
@@ -0,0 +1,200 @@
+window.REPORT_DATA = {
+  "generated": "May 28, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 51,
+    "downloaded": "2026-05-27T18:58:00.501294",
+    "row_count": 525431,
+    "filename": "address-points-2026-05-27.geojson",
+    "remote_last_modified": "Wed, 27 May 2026 11:41:57 GMT",
+    "remote_content_length": 589237090,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 52,
+    "downloaded": "2026-05-28T13:00:12.477990",
+    "row_count": 525435,
+    "filename": "address-points-2026-05-28.geojson",
+    "remote_last_modified": "Thu, 28 May 2026 11:43:40 GMT",
+    "remote_content_length": 589241491,
+    "skipped": 0
+  },
+  "old_date_friendly": "Wednesday, May 27, 2026",
+  "new_date_friendly": "Thursday, May 28, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 52,
+      "max_snapshot_id": 52,
+      "address_point_id": 60072141,
+      "address_full": "144A Bexhill Ave",
+      "address_number": "144A",
+      "lo_num": 144,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bexhill Ave",
+      "linear_name": "Bexhill",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.2919,
+      "latitude": 43.70884,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008717, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006329, \"CENTRELINE_ID\": 112386, \"CENTRELINE_MEASURE\": 91.41, \"CENTRELINE_OFFSET\": 23.62, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7357.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051904\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 52,
+      "max_snapshot_id": 52,
+      "address_point_id": 60072144,
+      "address_full": "56B Leyton Ave",
+      "address_number": "56B",
+      "lo_num": 56,
+      "lo_num_suf": "B",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Leyton Ave",
+      "linear_name": "Leyton",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.27872,
+      "latitude": 43.69482,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008723, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006331, \"CENTRELINE_ID\": 112914, \"CENTRELINE_MEASURE\": 58.96, \"CENTRELINE_OFFSET\": 24.64, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 8330.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051905\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 52,
+      "max_snapshot_id": 52,
+      "address_point_id": 60072142,
+      "address_full": "144B Bexhill Ave",
+      "address_number": "144B",
+      "lo_num": 144,
+      "lo_num_suf": "B",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bexhill Ave",
+      "linear_name": "Bexhill",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.29192,
+      "latitude": 43.70889,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008720, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006330, \"CENTRELINE_ID\": 112386, \"CENTRELINE_MEASURE\": 97.04, \"CENTRELINE_OFFSET\": 23.49, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7357.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051906\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 52,
+      "max_snapshot_id": 52,
+      "address_point_id": 60072145,
+      "address_full": "56 Leyton Ave",
+      "address_number": "56",
+      "lo_num": 56,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Leyton Ave",
+      "linear_name": "Leyton",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.27866,
+      "latitude": 43.69474,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008726, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 217964, \"CENTRELINE_ID\": 112914, \"CENTRELINE_MEASURE\": 49.31, \"CENTRELINE_OFFSET\": 21.86, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 8330.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051907\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [
+    {
+      "address_point_id": 14078278,
+      "address_full": "157 Hudson Dr",
+      "municipality_name": "former Toronto",
+      "latitude": 43.6929,
+      "longitude": -79.37844,
+      "changes": [
+        {
+          "field": "address_full",
+          "old": "151 Hudson Dr",
+          "new": "157 Hudson Dr",
+          "display_field": "Full Address"
+        },
+        {
+          "field": "address_number",
+          "old": "151",
+          "new": "157",
+          "display_field": "Address Number"
+        },
+        {
+          "field": "lo_num",
+          "old": 151,
+          "new": 157,
+          "display_field": "Low Number"
+        }
+      ]
+    }
+  ],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Scarborough": 4
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Scarborough Southwest": 4
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Full Address": 1,
+      "Address Number": 1,
+      "Low Number": 1
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 4,
+  "removed_count": 0,
+  "modified_count": 1,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      14,
+      0,
+      0,
+      0,
+      7,
+      0,
+      4
+    ],
+    "removed": [
+      2,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      1
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
