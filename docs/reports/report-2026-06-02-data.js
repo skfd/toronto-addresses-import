44ff369b59d9f9a020fe9b1d9fc44b90425aa6d3
skfd <kk@comentality.com>
2026-06-02 13:01:00 -0400

    data 2026-06-02

---
+++ docs/reports/report-2026-06-02-data.js
@@ -0,0 +1,126 @@
+window.REPORT_DATA = {
+  "generated": "Jun 02, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 53,
+    "downloaded": "2026-05-29T13:00:11.417496",
+    "row_count": 525435,
+    "filename": "address-points-2026-05-29.geojson",
+    "remote_last_modified": "Fri, 29 May 2026 11:42:58 GMT",
+    "remote_content_length": 589241384,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 54,
+    "downloaded": "2026-06-02T13:00:12.558712",
+    "row_count": 525437,
+    "filename": "address-points-2026-06-02.geojson",
+    "remote_last_modified": "Tue, 02 Jun 2026 11:40:52 GMT",
+    "remote_content_length": 589243498,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, May 29, 2026",
+  "new_date_friendly": "Tuesday, Jun 02, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 54,
+      "max_snapshot_id": 54,
+      "address_point_id": 60073095,
+      "address_full": "19 Boothroyd Ave",
+      "address_number": "19",
+      "lo_num": 19,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Boothroyd Ave",
+      "linear_name": "Boothroyd",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-Danforth",
+      "longitude": -79.33571,
+      "latitude": 43.67427,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008774, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006332, \"CENTRELINE_ID\": 1141370, \"CENTRELINE_MEASURE\": 91.04, \"CENTRELINE_OFFSET\": 25.17, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3033.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051908\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"14\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 54,
+      "max_snapshot_id": 54,
+      "address_point_id": 60073096,
+      "address_full": "21 Boothroyd Ave",
+      "address_number": "21",
+      "lo_num": 21,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Boothroyd Ave",
+      "linear_name": "Boothroyd",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-Danforth",
+      "longitude": -79.33572,
+      "latitude": 43.67433,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008777, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006333, \"CENTRELINE_ID\": 1141370, \"CENTRELINE_MEASURE\": 98.25, \"CENTRELINE_OFFSET\": 26.53, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3033.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051909\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"14\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 2
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Toronto-Danforth": 2
+    },
+    "ward_removed": {},
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 2,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      4,
+      0,
+      0,
+      0,
+      0,
+      2
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      1,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
