ac5ea297f4a9b25dd051ee3efeefebc4c9bc7439
skfd <kk@comentality.com>
2026-06-04 13:01:58 -0400

    data 2026-06-04

---
+++ docs/reports/report-2026-06-04-data.js
@@ -0,0 +1,269 @@
+window.REPORT_DATA = {
+  "generated": "Jun 04, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 54,
+    "downloaded": "2026-06-02T13:00:12.558712",
+    "row_count": 525437,
+    "filename": "address-points-2026-06-02.geojson",
+    "remote_last_modified": "Tue, 02 Jun 2026 11:40:52 GMT",
+    "remote_content_length": 589243498,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 55,
+    "downloaded": "2026-06-04T13:00:11.778361",
+    "row_count": 525435,
+    "filename": "address-points-2026-06-04.geojson",
+    "remote_last_modified": "Thu, 04 Jun 2026 11:40:45 GMT",
+    "remote_content_length": 589241257,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Jun 02, 2026",
+  "new_date_friendly": "Thursday, Jun 04, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 55,
+      "max_snapshot_id": 55,
+      "address_point_id": 60073101,
+      "address_full": "447 Horner Ave",
+      "address_number": "447",
+      "lo_num": 447,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Horner Ave",
+      "linear_name": "Horner",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54086,
+      "latitude": 43.60206,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008785, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006334, \"CENTRELINE_ID\": 60053733, \"CENTRELINE_MEASURE\": 118.08, \"CENTRELINE_OFFSET\": 68.25, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1823.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051910\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 55,
+      "max_snapshot_id": 55,
+      "address_point_id": 60073100,
+      "address_full": "445 Horner Ave",
+      "address_number": "445",
+      "lo_num": 445,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Horner Ave",
+      "linear_name": "Horner",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54036,
+      "latitude": 43.60217,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008782, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 4614, \"CENTRELINE_ID\": 60053733, \"CENTRELINE_MEASURE\": 75.63, \"CENTRELINE_OFFSET\": 67.86, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1823.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051911\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "history": []
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 54,
+      "address_point_id": 14210362,
+      "address_full": "709 Church St",
+      "address_number": "709",
+      "lo_num": 709,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Church St",
+      "linear_name": "Church",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "University-Rosedale",
+      "longitude": -79.38518,
+      "latitude": 43.67229,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1472838, \"ADDRESS_ID_LINK\": 610567, \"ADDRESS_POINT_ID_LINK\": 7950447, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 125496, \"CENTRELINE_ID\": 14021163, \"CENTRELINE_MEASURE\": 37.31, \"CENTRELINE_OFFSET\": 17.54, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"TORONTO\", \"LINEAR_NAME_ID\": 503.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"598230\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"11\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-04",
+          "kind": "removed",
+          "snapshot_id": 55,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 54,
+      "address_point_id": 7950451,
+      "address_full": "28 Park Rd",
+      "address_number": "28",
+      "lo_num": 28,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Park Rd",
+      "linear_name": "Park",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "University-Rosedale",
+      "longitude": -79.38497,
+      "latitude": 43.67228,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1472837, \"ADDRESS_ID_LINK\": 610569, \"ADDRESS_POINT_ID_LINK\": 7950445, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 125495, \"CENTRELINE_ID\": 7950454, \"CENTRELINE_MEASURE\": 16.1, \"CENTRELINE_OFFSET\": 13.12, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4149.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"754951\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"11\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-04",
+          "kind": "removed",
+          "snapshot_id": 55,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 54,
+      "address_point_id": 20103102,
+      "address_full": "156 Fallingbrook Rd",
+      "address_number": "156",
+      "lo_num": 156,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Fallingbrook Rd",
+      "linear_name": "Fallingbrook",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.27947,
+      "latitude": 43.68233,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1471539, \"ADDRESS_ID_LINK\": 753508, \"ADDRESS_POINT_ID_LINK\": 20103092, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 99664, \"CENTRELINE_ID\": 8246214, \"CENTRELINE_MEASURE\": 12.35, \"CENTRELINE_OFFSET\": 15.38, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7874.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"887995\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-04",
+          "kind": "removed",
+          "snapshot_id": 55,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 54,
+      "address_point_id": 14210361,
+      "address_full": "707 Church St",
+      "address_number": "707",
+      "lo_num": 707,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Church St",
+      "linear_name": "Church",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "University-Rosedale",
+      "longitude": -79.38508,
+      "latitude": 43.67227,
+      "extra": "{\"ADDRESS_CLASS\": \"SE\", \"ADDRESS_CLASS_DESC\": \"Structure Entrance\", \"ADDRESS_ID\": 1472839, \"ADDRESS_ID_LINK\": 610568, \"ADDRESS_POINT_ID_LINK\": 7950446, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 125500, \"CENTRELINE_ID\": 14021163, \"CENTRELINE_MEASURE\": 25.73, \"CENTRELINE_OFFSET\": 16.18, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10001, \"CLASS_FAMILY_DESC\": \"Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"TORONTO\", \"LINEAR_NAME_ID\": 503.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"985674\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"11\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-04",
+          "kind": "removed",
+          "snapshot_id": 55,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Etobicoke": 2
+    },
+    "muni_removed": {
+      "former Toronto": 3,
+      "Scarborough": 1
+    },
+    "ward_added": {
+      "Etobicoke-Lakeshore": 2
+    },
+    "ward_removed": {
+      "University-Rosedale": 3,
+      "Scarborough Southwest": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 2,
+  "removed_count": 4,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      2,
+      0,
+      2
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      4
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
