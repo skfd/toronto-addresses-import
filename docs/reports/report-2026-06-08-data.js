f77ed79ca5abc303e71da85360c60dcbc4914b48
skfd <kk@comentality.com>
2026-06-08 13:01:06 -0400

    data 2026-06-08

---
+++ docs/reports/report-2026-06-08-data.js
@@ -0,0 +1,159 @@
+window.REPORT_DATA = {
+  "generated": "Jun 08, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 56,
+    "downloaded": "2026-06-05T13:00:09.796634",
+    "row_count": 525435,
+    "filename": "address-points-2026-06-05.geojson",
+    "remote_last_modified": "Fri, 05 Jun 2026 11:44:18 GMT",
+    "remote_content_length": 589241200,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 57,
+    "downloaded": "2026-06-08T13:00:10.483107",
+    "row_count": 525437,
+    "filename": "address-points-2026-06-08.geojson",
+    "remote_last_modified": "Mon, 08 Jun 2026 14:16:55 GMT",
+    "remote_content_length": 589243400,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, Jun 05, 2026",
+  "new_date_friendly": "Monday, Jun 08, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 57,
+      "max_snapshot_id": 57,
+      "address_point_id": 60074288,
+      "address_full": "61 Colgate Ave",
+      "address_number": "61",
+      "lo_num": 61,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Colgate Ave",
+      "linear_name": "Colgate",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Toronto-Danforth",
+      "longitude": -79.34111,
+      "latitude": 43.66205,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008846, \"ADDRESS_ID_LINK\": 243775, \"ADDRESS_POINT_ID_LINK\": 7577029, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006336, \"CENTRELINE_ID\": 7577028, \"CENTRELINE_MEASURE\": 13.33, \"CENTRELINE_OFFSET\": 10.24, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 3199.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051912\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"14\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 57,
+      "max_snapshot_id": 57,
+      "address_point_id": 60073895,
+      "address_full": "98R Primrose Ave",
+      "address_number": "98R",
+      "lo_num": 98,
+      "lo_num_suf": "R",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Primrose Ave",
+      "linear_name": "Primrose",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Davenport",
+      "longitude": -79.44614,
+      "latitude": 43.67089,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008836, \"ADDRESS_ID_LINK\": 64125, \"ADDRESS_POINT_ID_LINK\": 8418189, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006335, \"CENTRELINE_ID\": 30068182, \"CENTRELINE_MEASURE\": 154.37, \"CENTRELINE_OFFSET\": 41.49, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"TORONTO\", \"LINEAR_NAME_ID\": 9781.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051913\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"09\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [
+    {
+      "address_point_id": 30123125,
+      "address_full": "303 Manning Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.65379,
+      "longitude": -79.41168,
+      "changes": [
+        {
+          "field": "address_full",
+          "old": "301 Manning Ave",
+          "new": "303 Manning Ave",
+          "display_field": "Full Address"
+        },
+        {
+          "field": "address_number",
+          "old": "301",
+          "new": "303",
+          "display_field": "Address Number"
+        },
+        {
+          "field": "lo_num",
+          "old": 301,
+          "new": 303,
+          "display_field": "Low Number"
+        }
+      ]
+    }
+  ],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 2
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Toronto-Danforth": 1,
+      "Davenport": 1
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Full Address": 1,
+      "Address Number": 1,
+      "Low Number": 1
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 2,
+  "removed_count": 0,
+  "modified_count": 1,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      2,
+      0,
+      2,
+      0,
+      0,
+      0,
+      2
+    ],
+    "removed": [
+      0,
+      0,
+      4,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      1
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
