e1b466af355bfd7211b91a3c59f97d55129cfe55
skfd <kk@comentality.com>
2026-06-09 13:01:56 -0400

    data 2026-06-09

---
+++ docs/reports/report-2026-06-09-data.js
@@ -0,0 +1,126 @@
+window.REPORT_DATA = {
+  "generated": "Jun 09, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 57,
+    "downloaded": "2026-06-08T13:00:10.483107",
+    "row_count": 525437,
+    "filename": "address-points-2026-06-08.geojson",
+    "remote_last_modified": "Mon, 08 Jun 2026 14:16:55 GMT",
+    "remote_content_length": 589243400,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 58,
+    "downloaded": "2026-06-09T13:00:14.328891",
+    "row_count": 525439,
+    "filename": "address-points-2026-06-09.geojson",
+    "remote_last_modified": "Tue, 09 Jun 2026 11:42:26 GMT",
+    "remote_content_length": 589245265,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, Jun 08, 2026",
+  "new_date_friendly": "Tuesday, Jun 09, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 58,
+      "max_snapshot_id": 58,
+      "address_point_id": 60074307,
+      "address_full": "2725 Brimley Rd",
+      "address_number": "2725",
+      "lo_num": 2725,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Brimley Rd",
+      "linear_name": "Brimley",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough North",
+      "longitude": -79.27722,
+      "latitude": 43.8064,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60008870, \"ADDRESS_ID_LINK\": 60008867, \"ADDRESS_POINT_ID_LINK\": 60074306, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006337, \"CENTRELINE_ID\": 30108578, \"CENTRELINE_MEASURE\": 69.72, \"CENTRELINE_OFFSET\": 36.15, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7435.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051914\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"23\"}",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 58,
+      "max_snapshot_id": 58,
+      "address_point_id": 60074306,
+      "address_full": "4610 Finch Ave E",
+      "address_number": "4610",
+      "lo_num": 4610,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Finch Ave E",
+      "linear_name": "Finch",
+      "linear_name_type": "Ave",
+      "linear_name_dir": "E",
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough North",
+      "longitude": -79.27705,
+      "latitude": 43.80633,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008867, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 506869, \"CENTRELINE_ID\": 60074289, \"CENTRELINE_MEASURE\": 43.59, \"CENTRELINE_OFFSET\": 59.89, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 5589.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051915\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"23\"}",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Scarborough": 2
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Scarborough North": 2
+    },
+    "ward_removed": {},
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 2,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      2,
+      0,
+      0,
+      0,
+      2,
+      2
+    ],
+    "removed": [
+      0,
+      4,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
