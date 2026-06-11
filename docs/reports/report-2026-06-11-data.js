8c8f1817aa4c07278fd08d7ce67a6a535db0bfd7
skfd <kk@comentality.com>
2026-06-11 13:01:21 -0400

    data 2026-06-11

---
+++ docs/reports/report-2026-06-11-data.js
@@ -0,0 +1,118 @@
+window.REPORT_DATA = {
+  "generated": "Jun 11, 2026 at 01:01 PM",
+  "old_snapshot": {
+    "id": 58,
+    "downloaded": "2026-06-09T13:00:14.328891",
+    "row_count": 525439,
+    "filename": "address-points-2026-06-09.geojson",
+    "remote_last_modified": "Tue, 09 Jun 2026 11:42:26 GMT",
+    "remote_content_length": 589245265,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 59,
+    "downloaded": "2026-06-11T13:00:15.229037",
+    "row_count": 525438,
+    "filename": "address-points-2026-06-11.geojson",
+    "remote_last_modified": "Thu, 11 Jun 2026 11:44:53 GMT",
+    "remote_content_length": 589244441,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Jun 09, 2026",
+  "new_date_friendly": "Thursday, Jun 11, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 58,
+      "address_point_id": 13968126,
+      "address_full": "17 Aylmer Ave",
+      "address_number": "17",
+      "lo_num": 17,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Aylmer Ave",
+      "linear_name": "Aylmer",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "University-Rosedale",
+      "longitude": -79.38671,
+      "latitude": 43.67388,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 124716, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 125643, \"CENTRELINE_ID\": 1141465, \"CENTRELINE_MEASURE\": 279.19, \"CENTRELINE_OFFSET\": 13.86, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 2930.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"645586\", \"PLACE_NAME\": \"None\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"11\"}",
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-11",
+          "kind": "removed",
+          "snapshot_id": 59,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "former Toronto": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "University-Rosedale": 1
+    },
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "sparklines": {
+    "added": [
+      2,
+      0,
+      0,
+      0,
+      2,
+      2,
+      0
+    ],
+    "removed": [
+      4,
+      0,
+      0,
+      0,
+      0,
+      0,
+      1
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
