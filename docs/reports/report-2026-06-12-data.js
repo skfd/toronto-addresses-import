260d0a370fd5954c61243c7ae38349f64e424a11
skfd <kk@comentality.com>
2026-06-12 12:01:08 -0400

    data 2026-06-12

---
+++ docs/reports/report-2026-06-12-data.js
@@ -0,0 +1,190 @@
+window.REPORT_DATA = {
+  "generated": "Jun 12, 2026 at 12:01 PM",
+  "old_snapshot": {
+    "id": 59,
+    "downloaded": "2026-06-11T13:00:15.229037",
+    "row_count": 525438,
+    "filename": "address-points-2026-06-11.geojson",
+    "remote_last_modified": "Thu, 11 Jun 2026 11:44:53 GMT",
+    "remote_content_length": 589244441,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 60,
+    "downloaded": "2026-06-12T12:00:29.355011",
+    "row_count": 525437,
+    "filename": "address-points-2026-06-12.geojson",
+    "remote_last_modified": "Fri, 12 Jun 2026 11:41:37 GMT",
+    "remote_content_length": 589243137,
+    "skipped": 0
+  },
+  "old_date_friendly": "Thursday, Jun 11, 2026",
+  "new_date_friendly": "Friday, Jun 12, 2026",
+  "added": [],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 59,
+      "address_point_id": 370933,
+      "address_full": "106 Park St",
+      "address_number": "106",
+      "lo_num": 106,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Park St",
+      "linear_name": "Park",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.2523,
+      "latitude": 43.71132,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 188153, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 202475, \"CENTRELINE_ID\": 112225, \"CENTRELINE_MEASURE\": 74.88, \"CENTRELINE_OFFSET\": 23.78, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 8649.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"872458\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-12",
+          "kind": "removed",
+          "snapshot_id": 60,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [
+    {
+      "address_point_id": 7074424,
+      "address_full": "2125 Lawrence Ave W",
+      "municipality_name": "York",
+      "latitude": 43.69859,
+      "longitude": -79.5181,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "Weston Lions Park",
+          "new": "Weston Lions Park, Weston Lions Recreation Arena",
+          "display_field": "Place Name"
+        }
+      ]
+    }
+  ],
+  "modified_address_number": [
+    {
+      "address_point_id": 8007868,
+      "address_full": "121B Fern Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.64493,
+      "longitude": -79.4455,
+      "changes": [
+        {
+          "field": "address_full",
+          "old": "121 1/2 Fern Ave",
+          "new": "121B Fern Ave",
+          "display_field": "Full Address"
+        },
+        {
+          "field": "address_number",
+          "old": "121 1/2",
+          "new": "121B",
+          "display_field": "Address Number"
+        },
+        {
+          "field": "lo_num_suf",
+          "old": "1/2",
+          "new": "B",
+          "display_field": "Low Number Suffix"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {
+      "Scarborough": 1
+    },
+    "ward_added": {},
+    "ward_removed": {
+      "Scarborough Southwest": 1
+    },
+    "field_changes": {
+      "Full Address": 1,
+      "Address Number": 1,
+      "Low Number Suffix": 1,
+      "Place Name": 1
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 1,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 1,
+  "modified_address_number_count": 1,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      2,
+      2,
+      0,
+      0
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      1,
+      1
+    ],
+    "modified": [
+      1,
+      0,
+      0,
+      2,
+      3,
+      11,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      1,
+      0,
+      0,
+      1,
+      3,
+      11,
+      1
+    ],
+    "modified_address_number": [
+      0,
+      0,
+      0,
+      1,
+      0,
+      0,
+      1
+    ]
+  }
+}
