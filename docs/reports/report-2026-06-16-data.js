f585a604bb052bf4ddc986594534401bcb0e6ae7
skfd <kk@comentality.com>
2026-06-16 12:01:09 -0400

    data 2026-06-16

---
+++ docs/reports/report-2026-06-16-data.js
@@ -0,0 +1,413 @@
+window.REPORT_DATA = {
+  "generated": "Jun 16, 2026 at 12:01 PM",
+  "old_snapshot": {
+    "id": 60,
+    "downloaded": "2026-06-12T12:00:29.355011",
+    "row_count": 525437,
+    "filename": "address-points-2026-06-12.geojson",
+    "remote_last_modified": "Fri, 12 Jun 2026 11:41:37 GMT",
+    "remote_content_length": 589243137,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 61,
+    "downloaded": "2026-06-16T12:00:27.234324",
+    "row_count": 525447,
+    "filename": "address-points-2026-06-16.geojson",
+    "remote_last_modified": "Tue, 16 Jun 2026 11:42:30 GMT",
+    "remote_content_length": 589254166,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, Jun 12, 2026",
+  "new_date_friendly": "Tuesday, Jun 16, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60076873,
+      "address_full": "221 Carlingview Dr",
+      "address_number": "221",
+      "lo_num": 221,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Carlingview Dr",
+      "linear_name": "Carlingview",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke North",
+      "longitude": -79.59366,
+      "latitude": 43.68467,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008970, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 24183, \"CENTRELINE_ID\": 8322667, \"CENTRELINE_MEASURE\": 428.91, \"CENTRELINE_OFFSET\": 70.1, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1297.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051916\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"01\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60076874,
+      "address_full": "231 Carlingview Dr",
+      "address_number": "231",
+      "lo_num": 231,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Carlingview Dr",
+      "linear_name": "Carlingview",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke North",
+      "longitude": -79.59405,
+      "latitude": 43.68499,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008972, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 24178, \"CENTRELINE_ID\": 8322667, \"CENTRELINE_MEASURE\": 471.83, \"CENTRELINE_OFFSET\": 51.88, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1297.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051917\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"01\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077315,
+      "address_full": "44 Wineva Ave",
+      "address_number": "44",
+      "lo_num": 44,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Wineva Ave",
+      "linear_name": "Wineva",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Beaches-East York",
+      "longitude": -79.29492,
+      "latitude": 43.66869,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009010, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 223016, \"CENTRELINE_ID\": 14066765, \"CENTRELINE_MEASURE\": 88.72, \"CENTRELINE_OFFSET\": 32.29, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4699.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051918\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"19\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077317,
+      "address_full": "56 Broadway Ave",
+      "address_number": "56",
+      "lo_num": 56,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Broadway Ave",
+      "linear_name": "Broadway",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Don Valley West",
+      "longitude": -79.39616,
+      "latitude": 43.71049,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60009011, \"ADDRESS_ID_LINK\": 106533, \"ADDRESS_POINT_ID_LINK\": 11541844, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006343, \"CENTRELINE_ID\": 7632816, \"CENTRELINE_MEASURE\": 245.02, \"CENTRELINE_OFFSET\": 37.37, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 45.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051919\", \"PLACE_NAME_ALL\": \"Proposed residential building\", \"WARD\": \"15\"}",
+      "place_name": "Proposed residential building",
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077307,
+      "address_full": "15 Westover Hill Rd",
+      "address_number": "15",
+      "lo_num": 15,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Westover Hill Rd",
+      "linear_name": "Westover Hill",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "York",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.43149,
+      "latitude": 43.6966,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008997, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006339, \"CENTRELINE_ID\": 7631, \"CENTRELINE_MEASURE\": 19.02, \"CENTRELINE_OFFSET\": 16.0, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 953.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"YK\", \"OBJECTID\": \"1051920\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077308,
+      "address_full": "17 Westover Hill Rd",
+      "address_number": "17",
+      "lo_num": 17,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Westover Hill Rd",
+      "linear_name": "Westover Hill",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "York",
+      "ward_name": "Toronto-St. Paul's",
+      "longitude": -79.43155,
+      "latitude": 43.69675,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009000, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006340, \"CENTRELINE_ID\": 7631, \"CENTRELINE_MEASURE\": 36.39, \"CENTRELINE_OFFSET\": 15.97, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 953.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"YK\", \"OBJECTID\": \"1051921\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"12\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077311,
+      "address_full": "93B Donside Dr",
+      "address_number": "93B",
+      "lo_num": 93,
+      "lo_num_suf": "B",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Donside Dr",
+      "linear_name": "Donside",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.2868,
+      "latitude": 43.70148,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009006, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006342, \"CENTRELINE_ID\": 112674, \"CENTRELINE_MEASURE\": 39.9, \"CENTRELINE_OFFSET\": 18.91, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7763.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051922\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60076972,
+      "address_full": "50 Cannon Rd",
+      "address_number": "50",
+      "lo_num": 50,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Cannon Rd",
+      "linear_name": "Cannon",
+      "linear_name_type": "Rd",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.48926,
+      "latitude": 43.63395,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60008986, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60004708, \"CENTRELINE_ID\": 60000275, \"CENTRELINE_MEASURE\": 273.99, \"CENTRELINE_OFFSET\": 28.34, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1291.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"1051923\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077310,
+      "address_full": "93A Donside Dr",
+      "address_number": "93A",
+      "lo_num": 93,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Donside Dr",
+      "linear_name": "Donside",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.28687,
+      "latitude": 43.70146,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009003, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006341, \"CENTRELINE_ID\": 112674, \"CENTRELINE_MEASURE\": 33.42, \"CENTRELINE_OFFSET\": 19.32, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 7763.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"1051924\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 61,
+      "max_snapshot_id": 61,
+      "address_point_id": 60077314,
+      "address_full": "42 Wineva Ave",
+      "address_number": "42",
+      "lo_num": 42,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Wineva Ave",
+      "linear_name": "Wineva",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Beaches-East York",
+      "longitude": -79.29479,
+      "latitude": 43.66873,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009009, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 223220, \"CENTRELINE_ID\": 14066765, \"CENTRELINE_MEASURE\": 89.32, \"CENTRELINE_OFFSET\": 20.58, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4699.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051925\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"19\"}",
+      "place_name": null,
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [
+    {
+      "address_point_id": 8211618,
+      "address_full": "230 Cassandra Blvd",
+      "municipality_name": "North York",
+      "latitude": 43.75469,
+      "longitude": -79.31917,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "Cassandra Park",
+          "new": "Cassandra Park, North York Tennis Association Club - East",
+          "display_field": "Place Name"
+        }
+      ]
+    },
+    {
+      "address_point_id": 4912459,
+      "address_full": "7 Reesor Rd",
+      "municipality_name": "Scarborough",
+      "latitude": 43.83692,
+      "longitude": -79.1877,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": null,
+          "new": "TRCA Lands ( 4)",
+          "display_field": "Place Name"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30151103,
+      "address_full": "219 Glengarry Ave",
+      "municipality_name": "former Toronto",
+      "latitude": 43.72339,
+      "longitude": -79.41344,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": null,
+          "new": "Glengarry Park",
+          "display_field": "Place Name"
+        }
+      ]
+    }
+  ],
+  "modified_address_number": [
+    {
+      "address_point_id": 30080788,
+      "address_full": "44 Magnolia Ave",
+      "municipality_name": "Scarborough",
+      "latitude": 43.72104,
+      "longitude": -79.25516,
+      "changes": [
+        {
+          "field": "address_full",
+          "old": "44A Magnolia Ave",
+          "new": "44 Magnolia Ave",
+          "display_field": "Full Address"
+        },
+        {
+          "field": "address_number",
+          "old": "44A",
+          "new": "44",
+          "display_field": "Address Number"
+        },
+        {
+          "field": "lo_num_suf",
+          "old": "A",
+          "new": null,
+          "display_field": "Low Number Suffix"
+        }
+      ]
+    }
+  ],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Etobicoke": 3,
+      "former Toronto": 3,
+      "York": 2,
+      "Scarborough": 2
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "Etobicoke North": 2,
+      "Beaches-East York": 2,
+      "Toronto-St. Paul's": 2,
+      "Scarborough Southwest": 2,
+      "Don Valley West": 1,
+      "Etobicoke-Lakeshore": 1
+    },
+    "ward_removed": {},
+    "field_changes": {
+      "Place Name": 3,
+      "Full Address": 1,
+      "Address Number": 1,
+      "Low Number Suffix": 1
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 10,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 3,
+  "modified_address_number_count": 1,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      10
+    ],
+    "removed": [
+      1,
+      1,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      11,
+      2,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      11,
+      1,
+      0,
+      0,
+      0,
+      0,
+      3
+    ],
+    "modified_address_number": [
+      0,
+      1,
+      0,
+      0,
+      0,
+      0,
+      1
+    ]
+  }
+}
