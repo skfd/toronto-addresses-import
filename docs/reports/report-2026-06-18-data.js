2a350fb0bf65f45cd8331790bfb6d72bfd5f4bcc
skfd <kk@comentality.com>
2026-06-18 12:01:05 -0400

    data 2026-06-18

---
+++ docs/reports/report-2026-06-18-data.js
@@ -0,0 +1,134 @@
+window.REPORT_DATA = {
+  "generated": "Jun 18, 2026 at 12:01 PM",
+  "old_snapshot": {
+    "id": 62,
+    "downloaded": "2026-06-17T12:00:44.342712",
+    "row_count": 525453,
+    "filename": "address-points-2026-06-17.geojson",
+    "remote_last_modified": "Wed, 17 Jun 2026 11:42:07 GMT",
+    "remote_content_length": 589260613,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 63,
+    "downloaded": "2026-06-18T12:00:29.104301",
+    "row_count": 525453,
+    "filename": "address-points-2026-06-18.geojson",
+    "remote_last_modified": "Thu, 18 Jun 2026 11:45:33 GMT",
+    "remote_content_length": 589260433,
+    "skipped": 0
+  },
+  "old_date_friendly": "Wednesday, Jun 17, 2026",
+  "new_date_friendly": "Thursday, Jun 18, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [
+    {
+      "address_point_id": 57543,
+      "address_full": "2213 Dufferin St",
+      "municipality_name": "York",
+      "latitude": 43.69128,
+      "longitude": -79.448,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "Fairbank Memorial Park",
+          "new": "Fairbank Memorial Park, Fairbank Memorial Community Centre",
+          "display_field": "Place Name"
+        }
+      ]
+    },
+    {
+      "address_point_id": 13969580,
+      "address_full": "50 Keywest Ave",
+      "municipality_name": "York",
+      "latitude": 43.69286,
+      "longitude": -79.44842,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "Fairbank Memorial Park",
+          "new": "Fairbank Memorial Park Outdoor Pool",
+          "display_field": "Place Name"
+        }
+      ]
+    }
+  ],
+  "modified_address_number": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Place Name": 2
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 2,
+  "modified_address_number_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      10,
+      7,
+      0
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      4,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      0,
+      0,
+      0,
+      0,
+      3,
+      0,
+      2
+    ],
+    "modified_address_number": [
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+      0
+    ]
+  }
+}
