b59995363380aa7d8b59cfa8f19e476170dfb632
skfd <kk@comentality.com>
2026-06-19 12:01:05 -0400

    data 2026-06-19

---
+++ docs/reports/report-2026-06-19-data.js
@@ -0,0 +1,119 @@
+window.REPORT_DATA = {
+  "generated": "Jun 19, 2026 at 12:01 PM",
+  "old_snapshot": {
+    "id": 63,
+    "downloaded": "2026-06-18T12:00:29.104301",
+    "row_count": 525453,
+    "filename": "address-points-2026-06-18.geojson",
+    "remote_last_modified": "Thu, 18 Jun 2026 11:45:33 GMT",
+    "remote_content_length": 589260433,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 64,
+    "downloaded": "2026-06-19T12:00:29.025465",
+    "row_count": 525453,
+    "filename": "address-points-2026-06-19.geojson",
+    "remote_last_modified": "Fri, 19 Jun 2026 11:48:02 GMT",
+    "remote_content_length": 589260311,
+    "skipped": 0
+  },
+  "old_date_friendly": "Thursday, Jun 18, 2026",
+  "new_date_friendly": "Friday, Jun 19, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [
+    {
+      "address_point_id": 9963198,
+      "address_full": "28 Dunlace Dr",
+      "municipality_name": "North York",
+      "latitude": 43.76266,
+      "longitude": -79.36859,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "Dunlace Park",
+          "new": "Dunlace Park, Dunlace Park Tennis Club",
+          "display_field": "Place Name"
+        }
+      ]
+    }
+  ],
+  "modified_address_number": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Place Name": 1
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 1,
+  "modified_address_number_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      10,
+      7,
+      0,
+      0
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      4,
+      0,
+      2,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      0,
+      0,
+      0,
+      3,
+      0,
+      2,
+      1
+    ],
+    "modified_address_number": [
+      0,
+      0,
+      0,
+      1,
+      0,
+      0,
+      0
+    ]
+  }
+}
