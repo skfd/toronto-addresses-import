7351995a400b0bb18d0522a27070421e97f8ecc0
skfd <kk@comentality.com>
2026-06-22 12:01:11 -0400

    data 2026-06-22

---
+++ docs/reports/report-2026-06-22-data.js
@@ -0,0 +1,128 @@
+window.REPORT_DATA = {
+  "generated": "Jun 22, 2026 at 12:01 PM",
+  "old_snapshot": {
+    "id": 64,
+    "downloaded": "2026-06-19T12:00:29.025465",
+    "row_count": 525453,
+    "filename": "address-points-2026-06-19.geojson",
+    "remote_last_modified": "Fri, 19 Jun 2026 11:48:02 GMT",
+    "remote_content_length": 589260311,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 65,
+    "downloaded": "2026-06-22T12:00:25.356851",
+    "row_count": 525454,
+    "filename": "address-points-2026-06-22.geojson",
+    "remote_last_modified": "Mon, 22 Jun 2026 14:16:16 GMT",
+    "remote_content_length": 589261686,
+    "skipped": 0
+  },
+  "old_date_friendly": "Friday, Jun 19, 2026",
+  "new_date_friendly": "Monday, Jun 22, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 65,
+      "max_snapshot_id": 65,
+      "address_point_id": 60078033,
+      "address_full": "1904 St Clair Ave W",
+      "address_number": "1904",
+      "lo_num": 1904,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "St Clair Ave W",
+      "linear_name": "St Clair",
+      "linear_name_type": "Ave",
+      "linear_name_dir": "W",
+      "municipality_name": "former Toronto",
+      "ward_name": "York South-Weston",
+      "longitude": -79.46612,
+      "latitude": 43.67303,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009098, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006344, \"CENTRELINE_ID\": 60014775, \"CENTRELINE_MEASURE\": 31.8, \"CENTRELINE_OFFSET\": 19.15, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 890.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"1051933\", \"PLACE_NAME_ALL\": \"Proposed pedestrian access to St. Clair-Old Weston SmartTrack Station\", \"WARD\": \"05\"}",
+      "place_name": "Proposed pedestrian access to St. Clair-Old Weston SmartTrack Station",
+      "history": []
+    }
+  ],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [],
+  "modified_address_number": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "former Toronto": 1
+    },
+    "muni_removed": {},
+    "ward_added": {
+      "York South-Weston": 1
+    },
+    "ward_removed": {},
+    "field_changes": {},
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 1,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 0,
+  "modified_address_number_count": 0,
+  "sparklines": {
+    "added": [
+      10,
+      7,
+      0,
+      0,
+      0,
+      0,
+      1
+    ],
+    "removed": [
+      0,
+      1,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      4,
+      0,
+      2,
+      1,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      3,
+      0,
+      2,
+      1,
+      0,
+      0,
+      0
+    ],
+    "modified_address_number": [
+      1,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
