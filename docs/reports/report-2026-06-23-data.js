c658f6fba51a45779f9dab89dfe1d3f8e790eb6b
skfd <kk@comentality.com>
2026-06-23 12:00:59 -0400

    data 2026-06-23

---
+++ docs/reports/report-2026-06-23-data.js
@@ -0,0 +1,134 @@
+window.REPORT_DATA = {
+  "generated": "Jun 23, 2026 at 12:00 PM",
+  "old_snapshot": {
+    "id": 65,
+    "downloaded": "2026-06-22T12:00:25.356851",
+    "row_count": 525454,
+    "filename": "address-points-2026-06-22.geojson",
+    "remote_last_modified": "Mon, 22 Jun 2026 14:16:16 GMT",
+    "remote_content_length": 589261686,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 66,
+    "downloaded": "2026-06-23T12:00:22.111249",
+    "row_count": 525454,
+    "filename": "address-points-2026-06-23.geojson",
+    "remote_last_modified": "Tue, 23 Jun 2026 11:40:14 GMT",
+    "remote_content_length": 589261406,
+    "skipped": 0
+  },
+  "old_date_friendly": "Monday, Jun 22, 2026",
+  "new_date_friendly": "Tuesday, Jun 23, 2026",
+  "added": [],
+  "removed": [],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [
+    {
+      "address_point_id": 30122594,
+      "address_full": "3 Hanover Rd",
+      "municipality_name": "North York",
+      "latitude": 43.73787,
+      "longitude": -79.46707,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": null,
+          "new": "Robert Leek Memorial Park",
+          "display_field": "Place Name"
+        }
+      ]
+    },
+    {
+      "address_point_id": 14077473,
+      "address_full": "3 Hanover Rd",
+      "municipality_name": "North York",
+      "latitude": 43.73797,
+      "longitude": -79.46671,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "Robert Leek Park,Hanover Park",
+          "new": "Robert Leek Memorial Park",
+          "display_field": "Place Name"
+        }
+      ]
+    }
+  ],
+  "modified_address_number": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {},
+    "muni_removed": {},
+    "ward_added": {},
+    "ward_removed": {},
+    "field_changes": {
+      "Place Name": 2
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 0,
+  "removed_count": 0,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 2,
+  "modified_address_number_count": 0,
+  "sparklines": {
+    "added": [
+      7,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0
+    ],
+    "removed": [
+      1,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified": [
+      0,
+      2,
+      1,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      0,
+      2,
+      1,
+      0,
+      0,
+      0,
+      2
+    ],
+    "modified_address_number": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
