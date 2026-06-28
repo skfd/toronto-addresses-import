e8c1cc53651f313a99ce932986b418ad2cc2fe38
skfd <kk@comentality.com>
2026-06-28 19:29:48 -0400

    data 2026-06-28

---
+++ docs/reports/report-2026-06-28-data.js
@@ -0,0 +1,325 @@
+window.REPORT_DATA = {
+  "generated": "Jun 28, 2026 at 07:29 PM",
+  "old_snapshot": {
+    "id": 66,
+    "downloaded": "2026-06-23T12:00:22.111249",
+    "row_count": 525454,
+    "filename": "address-points-2026-06-23.geojson",
+    "remote_last_modified": "Tue, 23 Jun 2026 11:40:14 GMT",
+    "remote_content_length": 589261406,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 67,
+    "downloaded": "2026-06-28T19:29:15.293301",
+    "row_count": 525451,
+    "filename": "toronto-2026-06-28.geojson",
+    "remote_last_modified": "Fri, 26 Jun 2026 11:42:06 GMT",
+    "remote_content_length": 589258096,
+    "skipped": 0
+  },
+  "old_date_friendly": "Tuesday, Jun 23, 2026",
+  "new_date_friendly": "Sunday, Jun 28, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 67,
+      "max_snapshot_id": 67,
+      "address_point_id": 60078384,
+      "address_full": "1 Monte Kwinter Crt",
+      "address_number": "1",
+      "lo_num": 1,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Monte Kwinter Crt",
+      "linear_name": "Monte Kwinter",
+      "linear_name_type": "Crt",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "York Centre",
+      "longitude": -79.447,
+      "latitude": 43.73338,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 60009214, \"ADDRESS_ID_LINK\": 60009213, \"ADDRESS_POINT_ID_LINK\": 30107732, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006345, \"CENTRELINE_ID\": 30123928, \"CENTRELINE_MEASURE\": 20.89, \"CENTRELINE_OFFSET\": 19.1, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 20977.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"1051934\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"06\"}",
+      "place_name": null,
+      "history": []
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 66,
+      "address_point_id": 11575348,
+      "address_full": "50 Bayview Wood",
+      "address_number": "50",
+      "lo_num": 50,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bayview Wood",
+      "linear_name": "Bayview",
+      "linear_name_type": "Wood",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Don Valley West",
+      "longitude": -79.38357,
+      "latitude": 43.72741,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 423507, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 440510, \"CENTRELINE_ID\": 30134619, \"CENTRELINE_MEASURE\": 54.45, \"CENTRELINE_OFFSET\": 16.6, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 11428.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"568725\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"15\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-28",
+          "kind": "removed",
+          "snapshot_id": 67,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 66,
+      "address_point_id": 355346,
+      "address_full": "141 Leyton Ave",
+      "address_number": "141",
+      "lo_num": 141,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Leyton Ave",
+      "linear_name": "Leyton",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "Scarborough",
+      "ward_name": "Scarborough Southwest",
+      "longitude": -79.27935,
+      "latitude": 43.69767,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 208732, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 218681, \"CENTRELINE_ID\": 112823, \"CENTRELINE_MEASURE\": 65.83, \"CENTRELINE_OFFSET\": 19.25, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11100, \"CLASS_FAMILY_DESC\": \"Land, Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 8330.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"SC\", \"OBJECTID\": \"844162\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"20\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-28",
+          "kind": "removed",
+          "snapshot_id": 67,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 66,
+      "address_point_id": 14200983,
+      "address_full": "58 McNairn Ave",
+      "address_number": "58",
+      "lo_num": 58,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "McNairn Ave",
+      "linear_name": "McNairn",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Eglinton-Lawrence",
+      "longitude": -79.40762,
+      "latitude": 43.73526,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 428106, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 638237, \"CENTRELINE_ID\": 1137583, \"CENTRELINE_MEASURE\": 200.16, \"CENTRELINE_OFFSET\": 38.25, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 20914.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"930642\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"08\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-28",
+          "kind": "removed",
+          "snapshot_id": 67,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 16,
+      "max_snapshot_id": 66,
+      "address_point_id": 840863,
+      "address_full": "60 McNairn Ave",
+      "address_number": "60",
+      "lo_num": 60,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "McNairn Ave",
+      "linear_name": "McNairn",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "former Toronto",
+      "ward_name": "Eglinton-Lawrence",
+      "longitude": -79.40771,
+      "latitude": 43.73525,
+      "extra": "{\"ADDRESS_CLASS\": \"S\", \"ADDRESS_CLASS_DESC\": \"Structure\", \"ADDRESS_ID\": 1462810, \"ADDRESS_ID_LINK\": 428106, \"ADDRESS_POINT_ID_LINK\": 14200983, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 638256, \"CENTRELINE_ID\": 1137583, \"CENTRELINE_MEASURE\": 200.16, \"CENTRELINE_OFFSET\": 44.07, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10011, \"CLASS_FAMILY_DESC\": \"Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 20914.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"TO\", \"OBJECTID\": \"934010\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"08\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-06-28",
+          "kind": "removed",
+          "snapshot_id": 67,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [],
+  "modified_place_name": [
+    {
+      "address_point_id": 7684061,
+      "address_full": "191 Winchester St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66695,
+      "longitude": -79.36271,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": null,
+          "new": "Cabbagetown Farmers' Market",
+          "display_field": "Place Name"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7640148,
+      "address_full": "635 Parliament St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66953,
+      "longitude": -79.36945,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": "St James Cemetery & Crmtrm",
+          "new": "St. James Cemetery & Crematorium",
+          "display_field": "Place Name"
+        }
+      ]
+    },
+    {
+      "address_point_id": 7684079,
+      "address_full": "201 Winchester St",
+      "municipality_name": "former Toronto",
+      "latitude": 43.66732,
+      "longitude": -79.36105,
+      "changes": [
+        {
+          "field": "place_name",
+          "old": null,
+          "new": "Riverdale Farm",
+          "display_field": "Place Name"
+        }
+      ]
+    }
+  ],
+  "modified_address_number": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "North York": 1
+    },
+    "muni_removed": {
+      "former Toronto": 2,
+      "North York": 1,
+      "Scarborough": 1
+    },
+    "ward_added": {
+      "York Centre": 1
+    },
+    "ward_removed": {
+      "Eglinton-Lawrence": 2,
+      "Don Valley West": 1,
+      "Scarborough Southwest": 1
+    },
+    "field_changes": {
+      "Place Name": 3
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 1,
+  "removed_count": 4,
+  "modified_count": 0,
+  "modified_location_count": 0,
+  "modified_place_name_count": 3,
+  "modified_address_number_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      1,
+      0,
+      0,
+      1
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      4
+    ],
+    "modified": [
+      1,
+      0,
+      0,
+      0,
+      2,
+      0,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "modified_place_name": [
+      1,
+      0,
+      0,
+      0,
+      2,
+      0,
+      3
+    ],
+    "modified_address_number": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]
+  }
+}
