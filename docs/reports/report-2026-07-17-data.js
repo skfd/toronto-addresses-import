0b89cbc66e07c4a7cf96b1192e3c98df63748c3c
skfd <kk@comentality.com>
2026-07-17 05:05:09 -0400

    data 2026-07-17

---
+++ docs/reports/report-2026-07-17-data.js
@@ -0,0 +1,333 @@
+window.REPORT_DATA = {
+  "generated": "Jul 17, 2026 at 05:05 AM",
+  "old_snapshot": {
+    "id": 68,
+    "downloaded": "2026-07-16T05:00:25.622825",
+    "row_count": 525472,
+    "filename": "toronto-2026-07-16.geojson",
+    "remote_last_modified": "Wed, 15 Jul 2026 11:42:14 GMT",
+    "remote_content_length": 589778738,
+    "skipped": 0
+  },
+  "new_snapshot": {
+    "id": 69,
+    "downloaded": "2026-07-17T05:00:18.151747",
+    "row_count": 525473,
+    "filename": "toronto-2026-07-17.geojson",
+    "remote_last_modified": "Thu, 16 Jul 2026 11:42:11 GMT",
+    "remote_content_length": 589779822,
+    "skipped": 0
+  },
+  "old_date_friendly": "Thursday, Jul 16, 2026",
+  "new_date_friendly": "Friday, Jul 17, 2026",
+  "added": [
+    {
+      "min_snapshot_id": 69,
+      "max_snapshot_id": 69,
+      "address_point_id": 60081106,
+      "address_full": "2539A Bayview Ave",
+      "address_number": "2539A",
+      "lo_num": 2539,
+      "lo_num_suf": "A",
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Bayview Ave",
+      "linear_name": "Bayview",
+      "linear_name_type": "Ave",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Don Valley West",
+      "longitude": -79.38354,
+      "latitude": 43.74719,
+      "extra": "{\"ADDRESS_CLASS\": \"LE\", \"ADDRESS_CLASS_DESC\": \"Land Entrance\", \"ADDRESS_ID\": 60009362, \"ADDRESS_ID_LINK\": 478180, \"ADDRESS_POINT_ID_LINK\": 20015342, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 501359, \"CENTRELINE_ID\": 30100672, \"CENTRELINE_MEASURE\": 117.81, \"CENTRELINE_OFFSET\": 37.83, \"CENTRELINE_SIDE\": \"R\", \"CLASS_FAMILY\": 10100, \"CLASS_FAMILY_DESC\": \"Land Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"TO, EY, NY\", \"LINEAR_NAME_ID\": 24.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"4242625\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"15\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 69,
+      "max_snapshot_id": 69,
+      "address_point_id": 60081729,
+      "address_full": "212 Beta St",
+      "address_number": "212",
+      "lo_num": 212,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Beta St",
+      "linear_name": "Beta",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54041,
+      "latitude": 43.6069,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009365, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 60006358, \"CENTRELINE_ID\": 7323999, \"CENTRELINE_MEASURE\": 183.46, \"CENTRELINE_OFFSET\": 26.09, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1163.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"4730042\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "place_name": null,
+      "history": []
+    },
+    {
+      "min_snapshot_id": 69,
+      "max_snapshot_id": 69,
+      "address_point_id": 60081728,
+      "address_full": "210 Beta St",
+      "address_number": "210",
+      "lo_num": 210,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Beta St",
+      "linear_name": "Beta",
+      "linear_name_type": "St",
+      "linear_name_dir": null,
+      "municipality_name": "Etobicoke",
+      "ward_name": "Etobicoke-Lakeshore",
+      "longitude": -79.54037,
+      "latitude": 43.60685,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 60009364, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 5031, \"CENTRELINE_ID\": 7323999, \"CENTRELINE_MEASURE\": 176.92, \"CENTRELINE_OFFSET\": 25.38, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 1000, \"CLASS_FAMILY_DESC\": \"Land\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 1163.0, \"MAINT_STAGE\": \"RESERVED\", \"MUNICIPALITY\": \"ET\", \"OBJECTID\": \"4730043\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"03\"}",
+      "place_name": null,
+      "history": []
+    }
+  ],
+  "removed": [
+    {
+      "min_snapshot_id": 68,
+      "max_snapshot_id": 68,
+      "address_point_id": 482824,
+      "address_full": "2 Anndale Dr",
+      "address_number": "2",
+      "lo_num": 2,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Anndale Dr",
+      "linear_name": "Anndale",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Willowdale",
+      "longitude": -79.40808,
+      "latitude": 43.76055,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 419154, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 434535, \"CENTRELINE_ID\": 30076010, \"CENTRELINE_MEASURE\": 104.48, \"CENTRELINE_OFFSET\": 6.35, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4803.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"3846337\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"18\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-07-17",
+          "kind": "removed",
+          "snapshot_id": 69,
+          "current": true
+        }
+      ]
+    },
+    {
+      "min_snapshot_id": 68,
+      "max_snapshot_id": 68,
+      "address_point_id": 482826,
+      "address_full": "4 Anndale Dr",
+      "address_number": "4",
+      "lo_num": 4,
+      "lo_num_suf": null,
+      "hi_num": null,
+      "hi_num_suf": null,
+      "linear_name_full": "Anndale Dr",
+      "linear_name": "Anndale",
+      "linear_name_type": "Dr",
+      "linear_name_dir": null,
+      "municipality_name": "North York",
+      "ward_name": "Willowdale",
+      "longitude": -79.40795,
+      "latitude": 43.76059,
+      "extra": "{\"ADDRESS_CLASS\": \"L\", \"ADDRESS_CLASS_DESC\": \"Land\", \"ADDRESS_ID\": 419153, \"ADDRESS_STATUS\": \"None\", \"ADDRESS_STRING_ID\": 434534, \"CENTRELINE_ID\": 30076010, \"CENTRELINE_MEASURE\": 114.28, \"CENTRELINE_OFFSET\": 13.12, \"CENTRELINE_SIDE\": \"L\", \"CLASS_FAMILY\": 11011, \"CLASS_FAMILY_DESC\": \"Land, Structure, Structure Entrance\", \"GENERAL_USE\": \"Unknown\", \"GENERAL_USE_CODE\": 115001, \"LINEAR_NAME_DESC\": \"None\", \"LINEAR_NAME_ID\": 4803.0, \"MAINT_STAGE\": \"REGULAR\", \"MUNICIPALITY\": \"NY\", \"OBJECTID\": \"3846541\", \"PLACE_NAME_ALL\": \"None\", \"WARD\": \"18\"}",
+      "place_name": null,
+      "history": [
+        {
+          "date": "2025-04-01",
+          "kind": "added",
+          "snapshot_id": 1,
+          "current": false
+        },
+        {
+          "date": "2026-07-17",
+          "kind": "removed",
+          "snapshot_id": 69,
+          "current": true
+        }
+      ]
+    }
+  ],
+  "modified": [],
+  "modified_location": [
+    {
+      "address_point_id": 3897789,
+      "address_full": "3 Faircrest Crcl",
+      "municipality_name": "East York",
+      "latitude": 43.70288,
+      "longitude": -79.3289,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70300, -79.32893",
+          "new": "43.70288, -79.32890",
+          "arrow": "\u2193 13.6m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 30066919,
+      "address_full": "4G Anndale Dr",
+      "municipality_name": "North York",
+      "latitude": 43.76066,
+      "longitude": -79.40812,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.76060, -79.40805",
+          "new": "43.76066, -79.40812",
+          "arrow": "\u2196 8.7m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 3897808,
+      "address_full": "4 Stinson Crcl",
+      "municipality_name": "East York",
+      "latitude": 43.70382,
+      "longitude": -79.3308,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70386, -79.33065",
+          "new": "43.70382, -79.33080",
+          "arrow": "\u2190 12.9m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 3897804,
+      "address_full": "2 Stinson Crcl",
+      "municipality_name": "East York",
+      "latitude": 43.70347,
+      "longitude": -79.33063,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70359, -79.33046",
+          "new": "43.70347, -79.33063",
+          "arrow": "\u2199 19.1m",
+          "display_field": "Location"
+        }
+      ]
+    },
+    {
+      "address_point_id": 8929176,
+      "address_full": "8 Alder Rd",
+      "municipality_name": "East York",
+      "latitude": 43.70242,
+      "longitude": -79.32827,
+      "changes": [
+        {
+          "field": "location",
+          "old": "43.70242, -79.32834",
+          "new": "43.70242, -79.32827",
+          "arrow": "\u2192 5.6m",
+          "display_field": "Location"
+        }
+      ]
+    }
+  ],
+  "modified_place_name": [],
+  "modified_address_number": [],
+  "new_streets": [],
+  "stats": {
+    "muni_added": {
+      "Etobicoke": 2,
+      "North York": 1
+    },
+    "muni_removed": {
+      "North York": 2
+    },
+    "ward_added": {
+      "Etobicoke-Lakeshore": 2,
+      "Don Valley West": 1
+    },
+    "ward_removed": {
+      "Willowdale": 2
+    },
+    "field_changes": {
+      "Location (longitude)": 5,
+      "Location (latitude)": 4
+    },
+    "top_streets_added": {},
+    "top_streets_removed": {}
+  },
+  "added_count": 3,
+  "removed_count": 2,
+  "modified_count": 0,
+  "modified_location_count": 5,
+  "modified_place_name_count": 0,
+  "modified_address_number_count": 0,
+  "sparklines": {
+    "added": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      29,
+      3
+    ],
+    "removed": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      8,
+      2
+    ],
+    "modified": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      2153,
+      0
+    ],
+    "modified_location": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      1521,
+      5
+    ],
+    "modified_place_name": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      472,
+      0
+    ],
+    "modified_address_number": [
+      0,
+      0,
+      0,
+      0,
+      0,
+      9,
+      0
+    ]
+  }
+}
